efc0e465105d4f52fc790bd38c7d494886cb5a05
Dennis Fantoni <dennis@fantoni.dk>
2013-10-08 16:45:59 +0200

    Updated release.cmd for the native dll to have date and time as part of the archive filename

--- native/tightdb_c_cs/tightdb_c_cs2012/release.cmd
+++ native/tightdb_c_cs/tightdb_c_cs2012/release.cmd
@@ -2,29 +2,48 @@
 echo Tightdb C# binding c++ Windows Build.
 Echo alpha 0.02
 echo ----------------------------------------------------------
-Echo Currently this batch file will create a release version of
-echo the already built source in the source directory used by
-echo the project residing where the batch 
-echo file resides. The release version will be placed in 
-echo (git checkout directory)\release\cppfiles\
-echo and (git checkout directory)\release\cpprelease\
-echo the directories are created if they does not exist.
-echo prior contents of release tree will be deleted, 
-echo except .7z files.
+echo press any key to populate release directory with
+echo a set of DLL files used by C# programs that access tightdb.
+echo .
+echo In general, relase.cmd will create release directories
+echo with release versions of the already built solution in the
+echo same directory as where the solution file is.
+echo The release version will be placed in a directory called
+echo release, in two sub directories files\ and release\
+echo files\ will be an uncompressed release of all needed files
+echo laid out in a logical way, and release\ will contain
+echo a zipped version of the files\directory - named according
+echo to the time the winrelease.cmd file was run
+echo the zipfile will also contain the username from the
+echo computer that generated the zipfile
 echo the contents of the directory will be c++ dll files
 echo in various builds.
+echo these dll's will contain methods called by the C# binding
+echo the dll's contain compiled core code
 echo This release is in effect our C interface release on windows
 echo It is consumed by the C# binding,but could also
-echo be used by bindings for other windows languages.
+echo be adapted to be used by bindings for other 
+echo windows languages. biggest task would be to create
+echo a header file, describing the contents of the dll
 echo ----------------------------------------------------------
 :echo parametres (%0)
 :echo where this file is located (%~dp0)
 :echo on
 
 set location=%~dp0
-set date=xdatex
-set time=xtimex
-set developer=xdeveloperx
+set reldate=%date:~6,4%-%date:~3,2%-%date:~0,2%
+set reltime=%time::=%
+set reltime=%reltime: =%
+set reltime=%reltime:,=%
+set reldeveloper=%USERNAME: =%
+set reldeveloper=%reldeveloper:,=%
+echo this batchfile is located at %location%
+echo -
+echo - release date will be set to %reldate%
+echo - release time will be set to %reltime%
+echo - release user will be set to %reldeveloper%
+echo -
+echo Press Any key to proceed, or close the window to abort
 set filesdestination=%location%release\files
 set releasedestination=%location%release\release
 set zipper=%location%..\..\..\7z.exe
@@ -59,7 +78,7 @@ copy %location%x64\debug\*.dll %filesdestination%
 copy %location%x64\release\*.dll %filesdestination%
 :create archive
 cd %filesdestination%
-%zipper% a -tzip -r %releasedestination%\tightdb_c_%xdatex%_%xtimex%_%xdeveloperx% *.*
+%zipper% a -tzip -r %releasedestination%\tightdb_c_%reldate%_%reltime%_%reldeveloper% *.*
 
 :revision history
 :0.01 
