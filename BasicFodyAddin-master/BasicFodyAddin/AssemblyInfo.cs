e2267700d6e13f63737b5db21723ad0fb1d1a67f
Kristian Dupont <kristian@kritiandupont.com>
2015-06-12 15:39:01 +0200

    Reordering folders and names

--- BasicFodyAddin-master/BasicFodyAddin/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("SampleFodyAddin")]
-[assembly: AssemblyProduct("SampleFodyAddin")]
-[assembly: AssemblyVersion("0.3.2")]
-[assembly: AssemblyFileVersion("0.3.2")]
---
+++ RealmWeaver/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("RealmWeaver")]
+[assembly: AssemblyProduct("RealmWeaver")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
