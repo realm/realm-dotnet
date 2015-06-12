abccdb3f84eb0b037b765b49e5aef6b55ae216e4
Kristian Dupont <kristian@kritiandupont.com>
2015-06-12 16:16:18 +0200

    Integration test uses query syntax

--- IntegrationTests/IntegrationTests.cs
+++ IntegrationTests/IntegrationTests.cs
@@ -21,22 +21,26 @@ namespace IntegrationTests
             p1.FirstName = "John";
             p1.LastName = "Smith";
             p1.IsInteresting = true;
+            p1.Email = "john@smith.com";
             Debug.WriteLine("p1 is named " + p1.FullName);
 
             var p2 = realm.CreateObject<Person>();
             p2.FullName = "John Doe";
             p2.IsInteresting = false;
+            p2.Email = "john@deo.com";
             Debug.WriteLine("p2 is named " + p2.FullName);
 
             var p3 = realm.CreateObject<Person>();
             p3.FullName = "Peter Jameson";
+            p3.Email = "peter@jameson.com";
             p3.IsInteresting = true;
             Debug.WriteLine("p3 is named " + p3.FullName);
 
+            var interestingPeople = from p in realm.All<Person>() where p.IsInteresting == true select p;
+
             Debug.WriteLine("Interesting people include:");
-            var peopleNamedJohn = realm.All<Person>().Where(p => p.IsInteresting == true);
-            foreach (var p in peopleNamedJohn)
-                Debug.WriteLine(" - " + p.FullName);
+            foreach (var p in interestingPeople)
+                Debug.WriteLine(" - " + p.FullName + " (" + p.Email + ")");
         }
     }
 }
