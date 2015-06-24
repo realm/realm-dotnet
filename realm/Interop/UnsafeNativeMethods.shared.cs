7b3a5ca9225cda6e43a0f79fd3de16ac7ab4277f
Kristian Dupont <kristian@kristiandupont.com>
2015-06-24 12:51:56 +0200

    Final unsafe native methods added

--- realm/Interop/UnsafeNativeMethods.shared.cs
+++ realm/Interop/UnsafeNativeMethods.shared.cs
@@ -36,7 +36,37 @@ public static class UnsafeNativeMethods
     [DllImport(InteropConfig.L64, EntryPoint="realm_get_ver_minor", CallingConvention = CallingConvention.Cdecl)]
     public static extern int GetMinorVer();
 
-    #region new_table
+    #region helpers
+
+    private static IntPtr StrAllocateBuffer(out long currentBufferSizeChars, long bufferSizeNeededChars)
+    {
+        currentBufferSizeChars = bufferSizeNeededChars;
+        return Marshal.AllocHGlobal((IntPtr) (bufferSizeNeededChars*sizeof (char)));
+        //allocHGlobal instead of  AllocCoTaskMem because allcHGlobal allows lt 2 gig on 64 bit (not that .net supports that right now, but at least this allocation will work with lt 32 bit strings)   
+    }
+
+    private static Boolean StrBufferOverflow(IntPtr buffer, long currentBufferSizeChars, long bufferSizeNeededChars)
+    {
+        if (currentBufferSizeChars < bufferSizeNeededChars)
+        {
+            Marshal.FreeHGlobal(buffer);
+
+            return true;
+        }
+        return false;
+    }
+
+    private static string StrBufToStr(IntPtr buffer, int bufferSizeNeededChars)
+    {
+        string retStr = bufferSizeNeededChars > 0 ? Marshal.PtrToStringUni(buffer, bufferSizeNeededChars) : "";
+        //return "" if the string is empty, otherwise copy data from the buffer
+        Marshal.FreeHGlobal(buffer);
+        return retStr;
+    }
+
+    #endregion
+
+    #region public static TableHandle new_table()
 
     //tightdb_c_cs_API size_t new_table()
     //The TableHandle will be initialized with root=null and that is exactly what we want
@@ -55,7 +85,7 @@ public static class UnsafeNativeMethods
 
     #endregion
 
-    #region table_add_column
+    #region public static long table_add_column(TableHandle tableHandle, DataType type, string name)
 
     [DllImport(InteropConfig.L64, EntryPoint = "table_add_column", CallingConvention = CallingConvention.Cdecl)]
     private static extern IntPtr table_add_column64(TableHandle tableHandle, IntPtr type,
@@ -75,7 +105,7 @@ public static class UnsafeNativeMethods
 
     #endregion
 
-    #region table_add_empty_row
+    #region public static long table_add_empty_row(TableHandle tableHandle, long numberOfRows)
 
     [DllImport(InteropConfig.L64, EntryPoint = "table_add_empty_row", CallingConvention = CallingConvention.Cdecl)]
     private static extern IntPtr table_add_empty_row64(TableHandle tableHandle, IntPtr numRows);
@@ -83,12 +113,71 @@ public static class UnsafeNativeMethods
     [DllImport(InteropConfig.L32, EntryPoint = "table_add_empty_row", CallingConvention = CallingConvention.Cdecl)]
     private static extern IntPtr table_add_empty_row32(TableHandle tableHandle, IntPtr numRows);
 
-    public static long table_add_empty_row32(TableHandle tableHandle, long numberOfRows)
+    public static long table_add_empty_row(TableHandle tableHandle, long numberOfRows)
     {
         if (InteropConfig.Is64Bit)
             return (long)table_add_empty_row64(tableHandle, (IntPtr)numberOfRows);
         else
             return (long)table_add_empty_row32(tableHandle, (IntPtr)numberOfRows);
+    }
+
+    #endregion
+
+    #region public static void table_set_string(TableHandle tableHandle, long columnIndex, long rowIndex, string value)
+
+    //        TIGHTDB_C_CS_API void table_set_int(Table* TablePtr, size_t column_ndx, size_t row_ndx, int64_t value)
+    [DllImport(InteropConfig.L64, EntryPoint = "table_set_string", CallingConvention = CallingConvention.Cdecl)]
+    private static extern void table_set_string64(TableHandle tablePtr, IntPtr columnNdx, IntPtr rowNdx,
+        [MarshalAs(UnmanagedType.LPWStr)] string value, IntPtr valueLen);
+
+    [DllImport(InteropConfig.L32, EntryPoint = "table_set_string", CallingConvention = CallingConvention.Cdecl)]
+    private static extern void table_set_string32(TableHandle tablePtr, IntPtr columnNdx, IntPtr rowNdx,
+        [MarshalAs(UnmanagedType.LPWStr)] string value, IntPtr valueLen);
+
+    public static void table_set_string(TableHandle tableHandle, long columnIndex, long rowIndex, string value)
+    {
+        if (InteropConfig.Is64Bit)
+            table_set_string64(tableHandle, (IntPtr)columnIndex, (IntPtr)rowIndex, value, (IntPtr)value.Length);
+        else
+            table_set_string32(tableHandle, (IntPtr)columnIndex, (IntPtr)rowIndex, value, (IntPtr)value.Length);
+    }
+
+    #endregion
+
+    #region public static string table_get_string(TableHandle tableHandle, long columnIndex, long rowIndex)
+
+    [DllImport(InteropConfig.L64, EntryPoint = "table_get_string", CallingConvention = CallingConvention.Cdecl)]
+    private static extern IntPtr table_get_string64(TableHandle handle, IntPtr columnIndex, IntPtr rowIndex,
+        IntPtr buffer, IntPtr bufsize);
+
+    [DllImport(InteropConfig.L32, EntryPoint = "table_get_string", CallingConvention = CallingConvention.Cdecl)]
+    private static extern IntPtr table_get_string32(TableHandle handle, IntPtr columnIndex, IntPtr rowIndex,
+        IntPtr buffer, IntPtr bufsize);
+    
+    public static string table_get_string(TableHandle tableHandle, long columnIndex, long rowIndex)
+    {
+        long bufferSizeNeededChars = 16;
+        IntPtr buffer;
+        long currentBufferSizeChars;
+
+        do
+        {
+            buffer = StrAllocateBuffer(out currentBufferSizeChars, bufferSizeNeededChars);
+
+            if (InteropConfig.Is64Bit)
+                bufferSizeNeededChars =
+                    (long)
+                    table_get_string64(tableHandle, (IntPtr)columnIndex, (IntPtr)rowIndex, buffer,
+                        (IntPtr) currentBufferSizeChars);
+
+            else
+                bufferSizeNeededChars =
+                    (long)
+                    table_get_string32(tableHandle, (IntPtr)columnIndex, (IntPtr)rowIndex, buffer,
+                        (IntPtr) currentBufferSizeChars);
+
+        } while (StrBufferOverflow(buffer, currentBufferSizeChars, bufferSizeNeededChars));
+        return StrBufToStr(buffer, (int) bufferSizeNeededChars);
     }
 
     #endregion
