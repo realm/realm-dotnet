ffe4d1d9fc412dc57e3f046bd1f8c83eb297378d
Kristian Dupont <kristian@kritiandupont.com>
2015-06-29 15:19:43 +0200

    Interop.Win32 project added

---
+++ realm/Interop/Interop.Win32/InteropConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Interop.Config
+{
+    public static class InteropConfig
+    {
+        public const bool Is64Bit = true;
+        public const string L32 = "(unimplemented)";
+        public const string L64 = "__Internal";
+    }
+}
