23f21198801af4899e1c65739f92c710664f5d19
Andy Dent <dent@oofile.com.au>
2015-07-28 18:14:20 +0800

    Fix build problems with #28 - CoreProviderFactory.shared.cs - take out Using clauses unused - roll back wrappers.vcxproj to Kristian's 9ee46d4

--- realm/Interop/CoreProviderFactory.shared.cs
+++ realm/Interop/CoreProviderFactory.shared.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using InteropShared;
+﻿using InteropShared;
 using RealmNet;
-using RealmNet.Interop;
 
 namespace Interop.Providers
 {
