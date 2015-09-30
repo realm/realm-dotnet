bb25d76319adefc5307fba2ff477a628f027ad27
Kristian Dupont <kristian@kristiandupont.com>
2015-09-30 15:31:22 +0200

    Split into Native* files

---
+++ realm/RealmNet.Shared/NativeSharedRealm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Interop.Config;
+
+namespace RealmNet
+{
+    internal static class NativeSharedRealm
+    {
+        [DllImport(InteropConfig.DLL_NAME, EntryPoint = "shared_realm_open", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr open(SchemaHandle schemaHandle, string path, IntPtr readOnly,
+            IntPtr durability, string encryptionKey);
+
+        [DllImport(InteropConfig.DLL_NAME, EntryPoint = "shared_realm_delete", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr delete(IntPtr sharedRealm);
+    }
+}
--- realm/wrappers/object-store/ffi.cpp
+++ realm/wrappers/object-store/ffi.cpp
@@ -67,38 +67,7 @@ extern "C" {
 
 using namespace realm;
 
-REALM_EXPORT void realm_error_destroy(RealmError* error)
-{
-    delete error;
-}
-
-REALM_EXPORT void realm_error_get_message(const RealmError* error, char const** out_message, size_t* out_message_size)
-{
-    *out_message = error->message.c_str();
-    *out_message_size = error->message.size();
-}
-
-REALM_EXPORT RealmErrorType realm_error_get_type(const RealmError* error)
-{
-    return error->type;
-}
-
-REALM_EXPORT std::vector<ObjectSchema>* realm_new_object_schemas()
-{
-    return new std::vector<ObjectSchema>();
-}
-
-REALM_EXPORT void realm_object_schemas_add_class(std::vector<ObjectSchema>* object_schemas, ObjectSchema* cls)
-{
-    object_schemas->push_back(*cls);
-}
-
-REALM_EXPORT Schema* realm_schema_new(ObjectSchema* object_schemas, size_t len)
-{
-    return new Schema(std::vector<ObjectSchema>(object_schemas, object_schemas + len * sizeof(ObjectSchema)));
-}
-
-REALM_EXPORT ObjectSchema* realm_object_schema_new(const char* name)
+REALM_EXPORT ObjectSchema* object_schema_new(const char* name)
 {
     std::cout << "Creating object schema '" << name << "'.\r\n";
 
@@ -107,7 +76,7 @@ REALM_EXPORT ObjectSchema* realm_object_schema_new(const char* name)
     return p;
 }
 
-REALM_EXPORT void realm_object_schema_add_property(ObjectSchema* cls, const char* name, DataType type, const char* object_type,
+REALM_EXPORT void object_schema_add_property(ObjectSchema* cls, const char* name, DataType type, const char* object_type,
                                       bool is_primary, bool is_indexed, bool is_nullable)
 {
     Property p;
@@ -120,7 +89,12 @@ REALM_EXPORT void realm_object_schema_add_property(ObjectSchema* cls, const char
     cls->properties.push_back(std::move(p));
 }
 
-REALM_EXPORT SharedRealm* realm_open(Schema* schema, const char* path, bool read_only, SharedGroup::DurabilityLevel durability,
+REALM_EXPORT Schema* schema_new(ObjectSchema* object_schemas, size_t len)
+{
+    return new Schema(std::vector<ObjectSchema>(object_schemas, object_schemas + len * sizeof(ObjectSchema)));
+}
+
+REALM_EXPORT SharedRealm* shared_realm_open(Schema* schema, const char* path, bool read_only, SharedGroup::DurabilityLevel durability,
                         const char* encryption_key)
 {
     Realm::Config config;
@@ -135,151 +109,151 @@ REALM_EXPORT SharedRealm* realm_open(Schema* schema, const char* path, bool read
     return new SharedRealm{Realm::get_shared_realm(config)};
 }
 
-REALM_EXPORT void realm_destroy(SharedRealm* realm)
+REALM_EXPORT void shared_realm_delete(SharedRealm* realm)
 {
     delete realm;
 }
 
-REALM_EXPORT bool realm_has_table(SharedRealm* realm, const char* name)
+REALM_EXPORT bool shared_realm_has_table(SharedRealm* realm, const char* name)
 {
     Group* g = (*realm)->read_group();
     return g->has_table(name);
 }
 
-REALM_EXPORT void realm_begin_transaction(SharedRealm* realm)
+REALM_EXPORT void shared_realm_begin_transaction(SharedRealm* realm)
 {
     handle_errors([&]() {
         (*realm)->begin_transaction();
     });
 }
 
-REALM_EXPORT void realm_commit_transaction(SharedRealm* realm)
+REALM_EXPORT void shared_realm_commit_transaction(SharedRealm* realm)
 {
     handle_errors([&]() {
         (*realm)->commit_transaction();
     });
 }
 
-REALM_EXPORT void realm_cancel_transaction(SharedRealm* realm)
+REALM_EXPORT void shared_realm_cancel_transaction(SharedRealm* realm)
 {
     handle_errors([&]() {
         (*realm)->cancel_transaction();
     });
 }
 
-REALM_EXPORT bool realm_is_in_transaction(SharedRealm* realm)
+REALM_EXPORT bool shared_realm_is_in_transaction(SharedRealm* realm)
 {
     return (*realm)->is_in_transaction();
 }
 
-REALM_EXPORT bool realm_refresh(SharedRealm* realm)
+REALM_EXPORT bool shared_realm_refresh(SharedRealm* realm)
 {
     return (*realm)->refresh();
 }
 
-REALM_EXPORT void realm_shared_group_promote_to_write(SharedGroup* sg, ClientHistory* hist)
-{
-    handle_errors([&]() {
-	    //LangBindHelper::promote_to_write(*sg, *hist);
-        transaction::begin(*sg, *hist, binding::delegate_instance);
-    });
-}
-
-REALM_EXPORT SharedGroup::version_type realm_shared_group_commit(SharedGroup* sg)
-{
-    return handle_errors([&]() {
-        return sg->commit();
-    });
-}
-
-REALM_EXPORT void realm_shared_group_commit_and_continue_as_read(SharedGroup* sg, ClientHistory* hist)
-{
-    //LangBindHelper::commit_and_continue_as_read(*sg);
-    transaction::commit(*sg, *hist, binding::delegate_instance);
-}
-
-REALM_EXPORT void realm_shared_group_rollback(SharedGroup* sg)
-{
-    sg->rollback();
-}
-
-REALM_EXPORT void realm_shared_group_rollback_and_continue_as_read(SharedGroup* sg, ClientHistory* hist)
-{
-    handle_errors([&]() {
-        //LangBindHelper::rollback_and_continue_as_read(*sg, *hist);
-        transaction::cancel(*sg, *hist, binding::delegate_instance);
-    });
-}
-
-REALM_EXPORT void realm_table_retain(const Table* table)
-{
-    LangBindHelper::bind_table_ptr(table);
-}
-
-REALM_EXPORT void realm_table_release(const Table* table)
-{
-    LangBindHelper::unbind_table_ptr(table);
-}
-
-REALM_EXPORT const Table* realm_get_table(SharedGroup* sg, const char* name)
-{
-    using sgf = _impl::SharedGroupFriend;
-    return handle_errors([&]() {
-        auto& group = sgf::get_group(*sg);
-        return LangBindHelper::get_table(group, name);
-    });
-}
-
-REALM_EXPORT Table* realm_get_table_mut(SharedGroup* sg, const char* name)
-{
-    using sgf = _impl::SharedGroupFriend;
-    return handle_errors([&]() {
-        auto& group = sgf::get_group(*sg);
-        return LangBindHelper::get_table(group, name);
-    });
-}
-
-REALM_EXPORT Table* realm_add_table(SharedGroup* sg, const char* name)
-{
-    using sgf = _impl::SharedGroupFriend;
-    return handle_errors([&]() {
-        auto& group = sgf::get_group(*sg);
-        return LangBindHelper::add_table(group, name);
-    });
-}
-
-REALM_EXPORT size_t realm_table_add_column(Table* table, DataType type, const char* name, bool nullable)
-{
-    return handle_errors([&]() {
-        return table->add_column(type, name, nullable);
-    });
-}
-
-REALM_EXPORT size_t realm_table_add_empty_row(Table* table)
-{
-    return handle_errors([&]() {
-        return table->add_empty_row();
-    });
-}
-
-REALM_EXPORT int64_t realm_table_get_int(const Table* table, size_t col_ndx, size_t row_ndx)
-{
-    return table->get_int(col_ndx, row_ndx);
-}
-
-REALM_EXPORT void realm_table_set_int(Table* table, size_t col_ndx, size_t row_ndx, int64_t value)
-{
-    return handle_errors([&]() {
-        table->set_int(col_ndx, row_ndx, value);
-    });
-}
-
-REALM_EXPORT void realm_row_destroy(Table::RowExpr* expr) {
-    delete expr;
-}
-
-REALM_EXPORT int64_t realm_row_get_int(const Table::RowExpr* expr, size_t col_ndx) {
-    return expr->get_int(col_ndx);
-}
+//REALM_EXPORT void shared_group_promote_to_write(SharedGroup* sg, ClientHistory* hist)
+//{
+//    handle_errors([&]() {
+//	    //LangBindHelper::promote_to_write(*sg, *hist);
+//        transaction::begin(*sg, *hist, binding::delegate_instance);
+//    });
+//}
+//
+//REALM_EXPORT SharedGroup::version_type shared_group_commit(SharedGroup* sg)
+//{
+//    return handle_errors([&]() {
+//        return sg->commit();
+//    });
+//}
+//
+//REALM_EXPORT void shared_group_commit_and_continue_as_read(SharedGroup* sg, ClientHistory* hist)
+//{
+//    //LangBindHelper::commit_and_continue_as_read(*sg);
+//    transaction::commit(*sg, *hist, binding::delegate_instance);
+//}
+//
+//REALM_EXPORT void shared_group_rollback(SharedGroup* sg)
+//{
+//    sg->rollback();
+//}
+//
+//REALM_EXPORT void shared_group_rollback_and_continue_as_read(SharedGroup* sg, ClientHistory* hist)
+//{
+//    handle_errors([&]() {
+//        //LangBindHelper::rollback_and_continue_as_read(*sg, *hist);
+//        transaction::cancel(*sg, *hist, binding::delegate_instance);
+//    });
+//}
+//
+//REALM_EXPORT void table_retain(const Table* table)
+//{
+//    LangBindHelper::bind_table_ptr(table);
+//}
+//
+//REALM_EXPORT void table_release(const Table* table)
+//{
+//    LangBindHelper::unbind_table_ptr(table);
+//}
+//
+//REALM_EXPORT const Table* shared_group_get_table(SharedGroup* sg, const char* name)
+//{
+//    using sgf = _impl::SharedGroupFriend;
+//    return handle_errors([&]() {
+//        auto& group = sgf::get_group(*sg);
+//        return LangBindHelper::get_table(group, name);
+//    });
+//}
+//
+//REALM_EXPORT Table* shared_group_get_table_mut(SharedGroup* sg, const char* name)
+//{
+//    using sgf = _impl::SharedGroupFriend;
+//    return handle_errors([&]() {
+//        auto& group = sgf::get_group(*sg);
+//        return LangBindHelper::get_table(group, name);
+//    });
+//}
+//
+//REALM_EXPORT Table* shared_group_add_table(SharedGroup* sg, const char* name)
+//{
+//    using sgf = _impl::SharedGroupFriend;
+//    return handle_errors([&]() {
+//        auto& group = sgf::get_group(*sg);
+//        return LangBindHelper::add_table(group, name);
+//    });
+//}
+//
+//REALM_EXPORT size_t table_add_column(Table* table, DataType type, const char* name, bool nullable)
+//{
+//    return handle_errors([&]() {
+//        return table->add_column(type, name, nullable);
+//    });
+//}
+//
+//REALM_EXPORT size_t table_add_empty_row(Table* table)
+//{
+//    return handle_errors([&]() {
+//        return table->add_empty_row();
+//    });
+//}
+//
+//REALM_EXPORT int64_t table_get_int(const Table* table, size_t col_ndx, size_t row_ndx)
+//{
+//    return table->get_int(col_ndx, row_ndx);
+//}
+//
+//REALM_EXPORT void table_set_int(Table* table, size_t col_ndx, size_t row_ndx, int64_t value)
+//{
+//    return handle_errors([&]() {
+//        table->set_int(col_ndx, row_ndx, value);
+//    });
+//}
+//
+//REALM_EXPORT void row_delete(Table::RowExpr* expr) {
+//    delete expr;
+//}
+//
+//REALM_EXPORT int64_t row_get_int(const Table::RowExpr* expr, size_t col_ndx) {
+//    return expr->get_int(col_ndx);
+//}
 
 }
