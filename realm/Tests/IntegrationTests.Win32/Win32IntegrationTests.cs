d0d7e576fee0ff5b0e3125609e00f398b2c53714
Kristian Dupont <kristian@kristiandupont.com>
2015-09-30 13:17:53 +0200

    First NativeRealm test..

--- realm/Tests/IntegrationTests.Win32/Win32IntegrationTests.cs
+++ realm/Tests/IntegrationTests.Win32/Win32IntegrationTests.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using NUnit.Framework;
 using RealmNet;
 
@@ -8,5 +9,15 @@ namespace IntegrationTests.Win32
     [TestFixture]
     public class Win32IntegrationTests : IntegrationTests
     {
+        [Test]
+        public void Tester()
+        {
+            var os1 = NativeRealm.realm_object_schema_new("123 hej med ☃ dig!");
+            var os2 = NativeRealm.realm_object_schema_new("Nummer 2");
+
+            var osses = new[] { os1, os2 };
+
+            NativeRealm.realm_schema_new(osses, (IntPtr) osses.Length);
+        }
     }
 }
