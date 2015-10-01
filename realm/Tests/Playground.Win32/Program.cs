a6199d5517c2288ab6d247f00a84781723bc6285
Kristian Dupont <kristian@kristiandupont.com>
2015-10-01 12:44:53 +0200

    Introduced Playground.Win32

---
+++ realm/Tests/Playground.Win32/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealmNet;
+
+namespace Playground.Win32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var os1 = NativeObjectSchema.object_schema_new("123 hej med dig ☃ <- snemand!");
+            var os2 = NativeObjectSchema.object_schema_new("Nummer 2");
+
+            var osses = new[] { os1, os2 };
+
+            NativeSchema.schema_new(osses, (IntPtr) osses.Length);
+        }
+    }
+}
