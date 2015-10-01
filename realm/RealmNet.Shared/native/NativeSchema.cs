ae52c2e53c3b523fc288500544c5bb1140263b8a
Kristian Dupont <kristian@kristiandupont.com>
2015-10-01 14:43:12 +0200

    Organize files into folders

---
+++ realm/RealmNet.Shared/native/NativeSchema.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Interop.Config;
+
+namespace RealmNet
+{
+    internal static class NativeSchema
+    {
+        [DllImport(InteropConfig.DLL_NAME, EntryPoint = "schema_new", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr schema_new([MarshalAs(UnmanagedType.LPArray, SizeParamIndex=1)] [In,Out] IntPtr[] objectSchemas, IntPtr objectSchemaCount);
+
+        [DllImport(InteropConfig.DLL_NAME, EntryPoint = "schema_generate", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr generate();
+    }
+}
---
+++ realm/RealmNet.Shared/native/NativeSharedRealm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Interop.Config;
+
+namespace RealmNet
+{
+    internal static class NativeSharedRealm
+    {
+        [DllImport(InteropConfig.DLL_NAME, EntryPoint = "shared_realm_open", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr open(SchemaHandle schemaHandle, string path, IntPtr readOnly,
+            IntPtr durability, string encryptionKey);
+
+        [DllImport(InteropConfig.DLL_NAME, EntryPoint = "shared_realm_delete", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr delete(IntPtr sharedRealm);
+
+        [DllImport(InteropConfig.DLL_NAME, EntryPoint = "shared_realm_has_table",
+            CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr has_table(SharedRealmHandle sharedRealm, string tableName);
+
+        [DllImport(InteropConfig.DLL_NAME, EntryPoint = "shared_realm_begin_transaction",
+            CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void begin_transaction(SharedRealmHandle sharedRealm);
+
+        [DllImport(InteropConfig.DLL_NAME, EntryPoint = "shared_realm_commit_transaction",
+            CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void commit_transaction(SharedRealmHandle sharedRealm);
+
+        [DllImport(InteropConfig.DLL_NAME, EntryPoint = "shared_realm_cancel_transaction",
+            CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void cancel_transaction(SharedRealmHandle sharedRealm);
+
+        [DllImport(InteropConfig.DLL_NAME, EntryPoint = "shared_realm_is_in_transaction",
+            CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr is_in_transaction(SharedRealmHandle sharedRealm);
+
+        [DllImport(InteropConfig.DLL_NAME, EntryPoint = "shared_realm_refresh",
+            CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void refresh(SharedRealmHandle sharedRealm);
+    }
+}
