cb030626cb5bcaa9c4b4f07d1b52b702899cbdbe
Kristian Dupont <kristian@kristiandupont.com>
2015-10-05 12:59:42 +0200

    Transaction class added (empty shell that "contains" a promote-to-write call)

--- realm/RealmNet.Shared/Transaction.cs
+++ realm/RealmNet.Shared/Transaction.cs
@@ -1,21 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace RealmNet
 {
     public class Transaction : IDisposable
     {
+        private SharedRealmHandle _sharedRealmHandle;
+        private bool _isOpen;
+
+        internal Transaction(SharedRealmHandle sharedRealmHandle)
+        {
+            this._sharedRealmHandle = sharedRealmHandle;
+            NativeSharedRealm.begin_transaction(sharedRealmHandle);
+            _isOpen = true;
+        }
+
         public void Dispose()
         {
+            if (!_isOpen)
+                return;
+
+            var exceptionOccurred = Marshal.GetExceptionPointers() != IntPtr.Zero || Marshal.GetExceptionCode() != 0;
+            if (exceptionOccurred)
+                Rollback();
+            else
+                Commit();
         }
 
         public void Rollback()
         {
+            if (!_isOpen)
+                throw new Exception("Transaction was already closed. Cannot roll back");
+
+            NativeSharedRealm.cancel_transaction(_sharedRealmHandle);
+            _isOpen = false;
         }
 
         public void Commit()
         {
+            if (!_isOpen)
+                throw new Exception("Transaction was already closed. Cannot commit");
+
+            NativeSharedRealm.commit_transaction(_sharedRealmHandle);
+            _isOpen = false;
         }
     }
 }
