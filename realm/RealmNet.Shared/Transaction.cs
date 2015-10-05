08be30a76ac3279bf540f83a257b0db6e56b3f41
Kristian Dupont <kristian@kristiandupont.com>
2015-10-05 10:39:14 +0200

    Transaction class "reintroduced"

---
+++ realm/RealmNet.Shared/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealmNet
+{
+    public class Transaction : IDisposable
+    {
+        public void Dispose()
+        {
+        }
+
+        public void Rollback()
+        {
+        }
+
+        public void Commit()
+        {
+        }
+    }
+}
