bafa35543d176dae64aa182c99453e299c9b1dc1
Kristian Dupont <kristian@kristiandupont.com>
2015-10-13 14:09:25 +0200

    Removed erroneous reliability contract from SharedRealmHandle

--- realm/RealmNet.Shared/handles/SharedRealmHandle.cs
+++ realm/RealmNet.Shared/handles/SharedRealmHandle.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.Runtime.CompilerServices;
-using System.Runtime.ConstrainedExecution;
-
-namespace RealmNet
+﻿namespace RealmNet
 {
     internal class SharedRealmHandle : RealmHandle
     {
         [Preserve("Constructor used by marshaling, cannot be removed by linker")]
-        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
         public SharedRealmHandle()
         {
         }
