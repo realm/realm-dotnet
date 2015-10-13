15190b3ddbf918fcece4fe866680a5c9571f0fd7
Kristian Dupont <kristian@kristiandupont.com>
2015-10-13 13:59:45 +0200

    Reliability contract added to ObjectSchemaHandle as well

--- realm/RealmNet.Shared/handles/ObjectSchemaHandle.cs
+++ realm/RealmNet.Shared/handles/ObjectSchemaHandle.cs
@@ -2,15 +2,17 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
 
 namespace RealmNet
 {
     internal class ObjectSchemaHandle : RealmHandle
     {
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
         public ObjectSchemaHandle(string name)
         {
             RuntimeHelpers.PrepareConstrainedRegions();
-            try { /**/ }
+            try { /* Retain handle in a constrained execution region */ }
             finally
             {
                 SetHandle(NativeObjectSchema.create(name));
