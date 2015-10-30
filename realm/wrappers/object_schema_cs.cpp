2608e7d231f3e905194807bee5811669d78cd4f3
Kristian Dupont <kristian@kristiandupont.com>
2015-10-30 13:54:49 +0100

    Wrap remaining functions in handle_errors

--- realm/wrappers/object_schema_cs.cpp
+++ realm/wrappers/object_schema_cs.cpp
@@ -28,27 +28,33 @@ extern "C" {
 
 REALM_EXPORT ObjectSchema* object_schema_create(const char* name)
 {
-    auto p = new ObjectSchema;
-    p->name = name;
-    return p;
+    return handle_errors([&]() {
+        auto p = new ObjectSchema;
+        p->name = name;
+        return p;
+    });
 }
 
 REALM_EXPORT void object_schema_destroy(ObjectSchema* object_schema)
 {
-    delete object_schema;
+    handle_errors([&]() {
+        delete object_schema;
+    });
 }
 
 REALM_EXPORT void object_schema_add_property(ObjectSchema* cls, const char* name, DataType type, const char* object_type,
                                       bool is_primary, bool is_indexed, bool is_nullable)
 {
-    Property p;
-    p.name = name;
-    p.type = static_cast<PropertyType>(type);
-    p.object_type = object_type ? std::string(object_type) : std::string();
-    p.is_primary = is_primary;
-    p.is_indexed = is_indexed;
-    p.is_nullable = is_nullable;
-    cls->properties.push_back(std::move(p));
+    handle_errors([&]() {
+        Property p;
+        p.name = name;
+        p.type = static_cast<PropertyType>(type);
+        p.object_type = object_type ? std::string(object_type) : std::string();
+        p.is_primary = is_primary;
+        p.is_indexed = is_indexed;
+        p.is_nullable = is_nullable;
+        cls->properties.push_back(std::move(p));
+    });
 }
 
 }   // extern "C"
