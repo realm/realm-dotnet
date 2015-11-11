dcfc11c8455ea8449987a96593479730918bc9e8
Andy Dent <dent@oofile.com.au>
2015-11-11 17:13:35 +0800

    Fixed spaces as per PR

--- Tests/IntegrationTests.Shared/IntegrationTests.cs
+++ Tests/IntegrationTests.Shared/IntegrationTests.cs
@@ -236,29 +236,29 @@ namespace IntegrationTests
         public void SearchComparingFloat()
         {
             MakeThreePeople (); 
-            var s0 = _realm.All<Person>().Where( p => p.Score == 42.42f).ToList();
+            var s0 = _realm.All<Person>().Where(p => p.Score == 42.42f).ToList();
             Assert.That(s0.Count, Is.EqualTo(1));
             Assert.That(s0[0].Score, Is.EqualTo(42.42f));
 
-            var s1 = _realm.All<Person>().Where( p => p.Score != 100.0f).ToList();
+            var s1 = _realm.All<Person>().Where(p => p.Score != 100.0f).ToList();
             Assert.That(s1.Count, Is.EqualTo(2));
             Assert.That(s1[0].Score, Is.EqualTo(-0.9907f));
             Assert.That(s1[1].Score, Is.EqualTo(42.42f));
 
-            var s2 = _realm.All<Person>().Where( p => p.Score < 0).ToList();
+            var s2 = _realm.All<Person>().Where(p => p.Score < 0).ToList();
             Assert.That(s2.Count, Is.EqualTo(1));
             Assert.That(s2[0].Score, Is.EqualTo(-0.9907f));
 
-            var s3 = _realm.All<Person>().Where( p => p.Score <= 42.42f).ToList();
+            var s3 = _realm.All<Person>().Where(p => p.Score <= 42.42f).ToList();
             Assert.That(s3.Count, Is.EqualTo(2));
             Assert.That(s3[0].Score, Is.EqualTo(-0.9907f));
             Assert.That(s3[1].Score, Is.EqualTo(42.42f));
 
-            var s4 = _realm.All<Person>().Where( p => p.Score > 99.0f).ToList();
+            var s4 = _realm.All<Person>().Where(p => p.Score > 99.0f).ToList();
             Assert.That(s4.Count, Is.EqualTo(1));
             Assert.That(s4[0].Score, Is.EqualTo(100.0f));
 
-            var s5 = _realm.All<Person>().Where( p => p.Score >= 100).ToList();
+            var s5 = _realm.All<Person>().Where(p => p.Score >= 100).ToList();
             Assert.That(s5.Count, Is.EqualTo(1));
             Assert.That(s5[0].Score, Is.EqualTo(100.0f));
         }
@@ -267,29 +267,29 @@ namespace IntegrationTests
         public void SearchComparingDouble()
         {
             MakeThreePeople (); 
-            var s0 = _realm.All<Person>().Where( p => p.Latitude == 40.7637286).ToList();
+            var s0 = _realm.All<Person>().Where(p => p.Latitude == 40.7637286).ToList();
             Assert.That(s0.Count, Is.EqualTo(1));
             Assert.That(s0[0].Latitude, Is.EqualTo(40.7637286));
 
-            var s1 = _realm.All<Person>().Where( p => p.Latitude != 40.7637286).ToList();
+            var s1 = _realm.All<Person>().Where(p => p.Latitude != 40.7637286).ToList();
             Assert.That(s1.Count, Is.EqualTo(2));
             Assert.That(s1[0].Latitude, Is.EqualTo(51.508530));
             Assert.That(s1[1].Latitude, Is.EqualTo(37.7798657));
 
-            var s2 = _realm.All<Person>().Where( p => p.Latitude < 40).ToList();
+            var s2 = _realm.All<Person>().Where(p => p.Latitude < 40).ToList();
             Assert.That(s2.Count, Is.EqualTo(1));
             Assert.That(s2[0].Latitude, Is.EqualTo(37.7798657));
 
-            var s3 = _realm.All<Person>().Where( p => p.Latitude <= 40.7637286).ToList();
+            var s3 = _realm.All<Person>().Where(p => p.Latitude <= 40.7637286).ToList();
             Assert.That(s3.Count, Is.EqualTo(2));
             Assert.That(s3[0].Latitude, Is.EqualTo(40.7637286));
             Assert.That(s3[1].Latitude, Is.EqualTo(37.7798657));
 
-            var s4 = _realm.All<Person>().Where( p => p.Latitude > 50).ToList();
+            var s4 = _realm.All<Person>().Where(p => p.Latitude > 50).ToList();
             Assert.That(s4.Count, Is.EqualTo(1));
             Assert.That(s4[0].Latitude, Is.EqualTo(51.508530));
 
-            var s5 = _realm.All<Person>().Where( p => p.Latitude >= 51.508530).ToList();
+            var s5 = _realm.All<Person>().Where(p => p.Latitude >= 51.508530).ToList();
             Assert.That(s5.Count, Is.EqualTo(1));
             Assert.That(s5[0].Latitude, Is.EqualTo(51.508530));
         }
