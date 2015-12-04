3b3a6b9631168de0a0c2903ad196149ece9d7209
Kristian Dupont <kristian@kristiandupont.com>
2015-12-04 15:11:20 +0100

    Manually reference all source files in Android.mk

--- wrappers/jni/Android.mk
+++ wrappers/jni/Android.mk
@@ -6,21 +6,36 @@ LOCAL_MODULE    := librealm-android
 LOCAL_SRC_FILES := ../core-android/$(TARGET_ARCH_ABI)/librealm-android.a
 include $(PREBUILT_STATIC_LIBRARY)
 
-#include $(CLEAR_VARS)
-
-OS_SRCS=$(wildcard ../object-store/*.cpp)
-ECH_SRCS=../object-store/impl/android/external_commit_helper.cpp
-SRCS=$(wildcard ../*.cpp)
-
+include $(CLEAR_VARS)
 LOCAL_MODULE := wrappers
 LOCAL_CFLAGS := 
-LOCAL_SRC_FILES := $(OS_SRCS)
-LOCAL_SRC_FILES += $(ECH_SRCS)
-LOCAL_SRC_FILES += $(SRCS)
+LOCAL_SRC_FILES := ../object-store/shared_realm.cpp
+LOCAL_SRC_FILES += ../object-store/index_set.cpp
+LOCAL_SRC_FILES += ../object-store/list.cpp
+LOCAL_SRC_FILES += ../object-store/object_schema.cpp
+LOCAL_SRC_FILES += ../object-store/object_store.cpp
+LOCAL_SRC_FILES += ../object-store/results.cpp
+LOCAL_SRC_FILES += ../object-store/schema.cpp
+LOCAL_SRC_FILES += ../object-store/impl/transact_log_handler.cpp
+LOCAL_SRC_FILES += ../object-store/impl/android/external_commit_helper.cpp
+
+LOCAL_SRC_FILES += ../error_handling.cpp
+LOCAL_SRC_FILES += ../linklist_cs.cpp
+LOCAL_SRC_FILES += ../marshalling.cpp
+LOCAL_SRC_FILES += ../object_schema_cs.cpp
+LOCAL_SRC_FILES += ../query_cs.cpp
+LOCAL_SRC_FILES += ../realm-csharp.cpp
+LOCAL_SRC_FILES += ../row_cs.cpp
+LOCAL_SRC_FILES += ../schema_cs.cpp
+LOCAL_SRC_FILES += ../shared_realm_cs.cpp
+LOCAL_SRC_FILES += ../table_cs.cpp
+
 LOCAL_LDLIBS := -llog
 LOCAL_LDLIBS += -lstdc++
 LOCAL_CPPFLAGS := -DHAVE_PTHREADS
 LOCAL_CPPFLAGS += -DREALM_HAVE_CONFIG=1
 LOCAL_C_INCLUDES += core-android/include
+LOCAL_C_INCLUDES += object-store/impl/
+LOCAL_C_INCLUDES += object-store/impl/android
 LOCAL_STATIC_LIBRARIES := realm-android
 include $(BUILD_SHARED_LIBRARY)
