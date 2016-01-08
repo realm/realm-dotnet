07a2f021e5911c7e87c68343797a8bc78666b947
Andy Dent <dent@oofile.com.au>
2016-01-08 21:32:15 +0800

    #298 Adding Count, Single, First to standard LINQ results
    SimpleLINQtests.cs
    - added
    
    PeopleTestsBase.cs
    - added
    
    Win32SimpleLINQtests.cs added
    
    ObjectIntegrationTests.cs
    - SearchComparingDouble, SearchComparingFloat all moved to SimpleLINQtests
    - refactored _realm and _databasePath, plus methods Setup, TearDown and
      MakeThreePeople to PeopleTestsBase
    - made subclass of PeopleTestsBase

---
+++ Tests/IntegrationTests.Shared/SimpleLINQtests.cs
@@ -0,0 +1,81 @@
+﻿/* Copyright 2015 Realm Inc - All Rights Reserved
+ * Proprietary and Confidential
+ */
+ 
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NUnit.Framework;
+namespace IntegrationTests
+{
+    class SimpleLINQtests : PeopleTestsBase
+    {
+
+
+        // Extension method rather than SQL-style LINQ
+        [Test]
+        public void SearchComparingFloat()
+        {
+            MakeThreePeople();
+            var s0a = _realm.All<Person>();
+            var s0b = s0a.Where(p => p.Score == 42.42f);
+            var s0 = s0b.ToList();
+            Assert.That(s0.Count, Is.EqualTo(1));
+            Assert.That(s0[0].Score, Is.EqualTo(42.42f));
+
+            var s1 = _realm.All<Person>().Where(p => p.Score != 100.0f).ToList();
+            Assert.That(s1.Count, Is.EqualTo(2));
+            Assert.That(s1[0].Score, Is.EqualTo(-0.9907f));
+            Assert.That(s1[1].Score, Is.EqualTo(42.42f));
+
+            var s2 = _realm.All<Person>().Where(p => p.Score < 0).ToList();
+            Assert.That(s2.Count, Is.EqualTo(1));
+            Assert.That(s2[0].Score, Is.EqualTo(-0.9907f));
+
+            var s3 = _realm.All<Person>().Where(p => p.Score <= 42.42f).ToList();
+            Assert.That(s3.Count, Is.EqualTo(2));
+            Assert.That(s3[0].Score, Is.EqualTo(-0.9907f));
+            Assert.That(s3[1].Score, Is.EqualTo(42.42f));
+
+            var s4 = _realm.All<Person>().Where(p => p.Score > 99.0f).ToList();
+            Assert.That(s4.Count, Is.EqualTo(1));
+            Assert.That(s4[0].Score, Is.EqualTo(100.0f));
+
+            var s5 = _realm.All<Person>().Where(p => p.Score >= 100).ToList();
+            Assert.That(s5.Count, Is.EqualTo(1));
+            Assert.That(s5[0].Score, Is.EqualTo(100.0f));
+        }
+
+        [Test]
+        public void SearchComparingDouble()
+        {
+            MakeThreePeople();
+            var s0 = _realm.All<Person>().Where(p => p.Latitude == 40.7637286).ToList();
+            Assert.That(s0.Count, Is.EqualTo(1));
+            Assert.That(s0[0].Latitude, Is.EqualTo(40.7637286));
+
+            var s1 = _realm.All<Person>().Where(p => p.Latitude != 40.7637286).ToList();
+            Assert.That(s1.Count, Is.EqualTo(2));
+            Assert.That(s1[0].Latitude, Is.EqualTo(51.508530));
+            Assert.That(s1[1].Latitude, Is.EqualTo(37.7798657));
+
+            var s2 = _realm.All<Person>().Where(p => p.Latitude < 40).ToList();
+            Assert.That(s2.Count, Is.EqualTo(1));
+            Assert.That(s2[0].Latitude, Is.EqualTo(37.7798657));
+
+            var s3 = _realm.All<Person>().Where(p => p.Latitude <= 40.7637286).ToList();
+            Assert.That(s3.Count, Is.EqualTo(2));
+            Assert.That(s3[0].Latitude, Is.EqualTo(40.7637286));
+            Assert.That(s3[1].Latitude, Is.EqualTo(37.7798657));
+
+            var s4 = _realm.All<Person>().Where(p => p.Latitude > 50).ToList();
+            Assert.That(s4.Count, Is.EqualTo(1));
+            Assert.That(s4[0].Latitude, Is.EqualTo(51.508530));
+
+            var s5 = _realm.All<Person>().Where(p => p.Latitude >= 51.508530).ToList();
+            Assert.That(s5.Count, Is.EqualTo(1));
+            Assert.That(s5[0].Latitude, Is.EqualTo(51.508530));
+        }
+    }
+}
---
+++ Tests/IntegrationTests.Win32/Win32SimpleLINQtests.cs
@@ -0,0 +1,12 @@
+﻿/* Copyright 2015 Realm Inc - All Rights Reserved
+ * Proprietary and Confidential
+ */
+
+using NUnit.Framework;
+
+namespace IntegrationTests.Win32 {
+
+    [TestFixture]
+    class Win32SimpleLINQtests : SimpleLINQtests {
+    }
+}
