0cad58d4a454138c43b3751d473ec0f876eba5c3
Kristian Dupont <kristian@kristiandupont.com>
2016-01-11 14:33:51 +0100

    Remove GetValue and SetValue that are now replaced.

--- Realm.Shared/RealmObject.cs
+++ Realm.Shared/RealmObject.cs
@@ -80,65 +80,6 @@ namespace Realms
             }
         }
 
-        protected T GetValue<T>(string propertyName)
-        {
-            if (_realm == null)
-                throw new Exception("This object is not managed. Create through CreateObject");
-
-            var tableHandle = _realm._tableHandles[GetType()];
-            var columnIndex = NativeTable.get_column_index(tableHandle, propertyName, (IntPtr)propertyName.Length);
-            var rowIndex = _rowHandle.RowIndex;
-
-            if (typeof(T) == typeof(string))
-            {
-                long bufferSizeNeededChars = 16;
-                IntPtr buffer;
-                long currentBufferSizeChars;
-
-                do
-                {
-                    buffer = MarshalHelpers.StrAllocateBuffer(out currentBufferSizeChars, bufferSizeNeededChars);
-                    bufferSizeNeededChars = (long)NativeTable.get_string(tableHandle, columnIndex, (IntPtr)rowIndex, buffer,
-                            (IntPtr)currentBufferSizeChars);
-
-                } while (MarshalHelpers.StrBufferOverflow(buffer, currentBufferSizeChars, bufferSizeNeededChars));
-                return (T)Convert.ChangeType(MarshalHelpers.StrBufToStr(buffer, (int)bufferSizeNeededChars), typeof(T));
-            }
-            if (typeof(T) == typeof(bool))
-            {
-                var value = MarshalHelpers.IntPtrToBool( NativeTable.get_bool(tableHandle, columnIndex, (IntPtr)rowIndex) );
-                return (T)Convert.ChangeType(value, typeof(T));
-            }
-            if (typeof(T) == typeof(int))  // System.Int32 regardless of process being 32 or 64bit
-            {
-                var value = NativeTable.get_int64(tableHandle, columnIndex, (IntPtr)rowIndex);
-                return (T)Convert.ChangeType(value, typeof(T));
-            }
-            if (typeof(T) == typeof(Int64)) 
-            {
-                var value = NativeTable.get_int64(tableHandle, columnIndex, (IntPtr)rowIndex);
-                return (T)Convert.ChangeType(value, typeof(T));
-            }
-            if (typeof(T) == typeof(float)) 
-            {
-                var value = NativeTable.get_float(tableHandle, columnIndex, (IntPtr)rowIndex);
-                return (T)Convert.ChangeType(value, typeof(T));
-            }
-            if (typeof(T) == typeof(double)) 
-            {
-                var value = NativeTable.get_double(tableHandle, columnIndex, (IntPtr)rowIndex);
-                return (T)Convert.ChangeType(value, typeof(T));
-            }
-            if (typeof(T) == typeof(DateTimeOffset))
-            {
-                var unixTimeSeconds = NativeTable.get_datetime_seconds(tableHandle, columnIndex, (IntPtr)rowIndex);
-                var value = DateTimeOffsetExtensions.FromUnixTimeSeconds(unixTimeSeconds);
-                return (T)(object)value;
-            }
-            else
-                throw new Exception ("Unsupported type " + typeof(T).Name);
-        }
-
         protected string GetStringValue(string propertyName)
         {
             if (_realm == null)
@@ -245,57 +186,6 @@ namespace Realms
             var columnIndex = NativeTable.get_column_index(tableHandle, propertyName, (IntPtr)propertyName.Length);
             var listHandle = tableHandle.TableLinkList (columnIndex, _rowHandle);
             return new RealmList<T>(this, listHandle);
-        }
-
-        protected void SetValue<T>(string propertyName, T value)
-        {
-            if (_realm == null)
-                throw new Exception("This object is not managed. Create through CreateObject");
-
-            if (!_realm.IsInTransaction)
-                throw new RealmOutsideTransactionException("Cannot set values outside transaction");
-
-            var tableHandle = _realm._tableHandles[GetType()];
-            var columnIndex = NativeTable.get_column_index(tableHandle, propertyName, (IntPtr)propertyName.Length);
-            var rowIndex = _rowHandle.RowIndex;
-
-            if (typeof(T) == typeof(string)) 
-            {
-                var str = value as string;
-                NativeTable.set_string (tableHandle, columnIndex, (IntPtr)rowIndex, str, (IntPtr)(str?.Length ?? 0));
-            } 
-            else if (typeof(T) == typeof(bool)) 
-            {
-                var marshalledValue = MarshalHelpers.BoolToIntPtr ((bool)Convert.ChangeType (value, typeof(bool)));
-                NativeTable.set_bool (tableHandle, columnIndex, (IntPtr)rowIndex, marshalledValue);
-            } 
-            else if (typeof(T) == typeof(int)) 
-            {  // System.Int32 regardless of process being 32 or 64bit
-                Int64 marshalledValue = Convert.ToInt64 (value);
-                NativeTable.set_int64 (tableHandle, columnIndex, (IntPtr)rowIndex, marshalledValue);
-            } 
-            else if (typeof(T) == typeof(Int64)) 
-            {
-                Int64 marshalledValue = Convert.ToInt64 (value);
-                NativeTable.set_int64 (tableHandle, columnIndex, (IntPtr)rowIndex, marshalledValue);
-            } 
-            else if (typeof(T) == typeof(float)) 
-            {
-                float marshalledValue = Convert.ToSingle (value);
-                NativeTable.set_float (tableHandle, columnIndex, (IntPtr)rowIndex, marshalledValue);
-            } 
-            else if (typeof(T) == typeof(double)) 
-            {
-                double marshalledValue = Convert.ToDouble (value);
-                NativeTable.set_double (tableHandle, columnIndex, (IntPtr)rowIndex, marshalledValue);
-            }
-            else if (typeof(T) == typeof(DateTimeOffset))
-            {
-                Int64 marshalledValue = ((DateTimeOffset)(object)value).ToUnixTimeSeconds();
-                NativeTable.set_datetime_seconds(tableHandle, columnIndex, (IntPtr)rowIndex, marshalledValue);
-            }
-            else
-                throw new Exception ("Unsupported type " + typeof(T).Name);
         }
 
         protected void SetStringValue(string propertyName, string value, bool setUnique)
