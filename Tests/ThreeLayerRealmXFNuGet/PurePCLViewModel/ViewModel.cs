8daeb8cf744e510af2499337324eb34403eebbe1
Andy Dent <dent@oofile.com.au>
2016-02-18 17:14:43 +0800

    Added variant of ThreeLayerRealmXF with NuGet to supply Realm rather than local DLLs (checkin without Android as need to recreate project for unique key)

---
+++ Tests/ThreeLayerRealmXFNuGet/PurePCLViewModel/ViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using Realms;
+
+namespace PurePCLViewModel
+{
+
+    class Hero : RealmObject
+    {
+        public string SuperName { get; set; }
+        public int SuperScore { get; set; }
+    }
+
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public string TheAnswer { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModel()
+        {
+        }
+
+        public void TestRealm()
+        {
+            Realm.DeleteRealm(RealmConfiguration.DefaultConfiguration);  // cleanup previous test run
+            var _realm = Realm.GetInstance();
+            using (var trans = _realm.BeginWrite()) {
+                for (int i = 0; i < 10; ++i)  // quick loop to add a few objects
+                {
+                    var pclPerson = _realm.CreateObject<Hero>();
+                    pclPerson.SuperName = $"Thor {i}";
+                    pclPerson.SuperScore = 10 * i;
+                }
+                trans.Commit();
+            }  // transaction wrapping add loop
+            var numAwe = _realm.All<Hero>().Count();
+
+            var timeStamp = DateTimeOffset.Now.ToString();
+            var sb = new StringBuilder();
+            foreach (var aThor in _realm.All<Hero>())
+                sb.AppendLine(aThor.SuperName);  // get the names back out to prove that data saved
+
+            // finished with Realm now
+            _realm.Close();
+
+            TheAnswer = $"{timeStamp}\n{numAwe} realm objects created:\n{sb.ToString()}";
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs("TheAnswer"));  // normally woudl be setter
+        }
+    }
+}
---
+++ Tests/ThreeLayerRealmXFNuGet/ThreeLayerRealmXFNuGet/ThreeLayerRealmXFNuGet.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,12 @@
+﻿using ThreeLayerRealmXF.WinPhone.Resources;
+
+namespace ThreeLayerRealmXF.WinPhone {
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
