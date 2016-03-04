77bb5ea4cafd0c0764299900163f698e8865be47
Kristian Dupont <kristian@kristiandupont.com>
2016-03-04 15:16:17 +0100

    Add notification test

---
+++ Tests/IntegrationTests.Shared/NotificationTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using Realms;
+using System.Threading;
+
+namespace IntegrationTests.Shared
+{
+    [TestFixture]
+    public class NotificationTests
+    {
+        private string _databasePath;
+        private Realm _realm;
+
+        private void WriteOnDifferentThread(Action<Realm> action)
+        {
+            var thread = new Thread(() =>
+            {
+                var r = Realm.GetInstance(_databasePath);
+                r.Write(() => action(r));
+                r.Close();
+            });
+            thread.Start();
+            thread.Join();
+        }
+
+        [Test]
+        public void SimpleTest() 
+        {
+            // Arrange
+
+
+            // Act
+
+        }
+    }
+}
+
