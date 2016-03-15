49b66935328153a44ef0b0c953ab1d2e41ee3edb
Yavor Georgiev <fealebenpae@gmail.com>
2016-03-15 15:21:43 +0100

    Add analytics rationale comment

--- RealmWeaver/Analytics.cs
+++ RealmWeaver/Analytics.cs
@@ -12,6 +12,32 @@ using Mono.Cecil;
 
 namespace RealmWeaver
 {
+    // Asynchronously submits build information to Realm when the assembly weaver
+    // is running
+    //
+    // To be clear: this does *not* run when your app is in production or on
+    // your end-user's devices; it will only run when you build your app from source.
+    //
+    // Why are we doing this? Because it helps us build a better product for you.
+    // None of the data personally identifies you, your employer or your app, but it
+    // *will* help us understand what Realm version you use, what host OS you use,
+    // etc. Having this info will help with prioritizing our time, adding new
+    // features and deprecating old features. Collecting an anonymized assembly name &
+    // anonymized MAC is the only way for us to count actual usage of the other
+    // metrics accurately. If we don't have a way to deduplicate the info reported,
+    // it will be useless, as a single developer building their app on Windows ten
+    // times would report 10 times more than a single developer that only builds
+    // once from Mac OS X, making the data all but useless. No one likes sharing
+    // data unless it's necessary, we get it, and we've debated adding this for a
+    // long long time. Since Realm is a free product without an email signup, we
+    // feel this is a necessary step so we can collect relevant data to build a
+    // better product for you.
+    //
+    // Currently the following information is reported:
+    // - What version of Realm is being used
+    // - What OS you are running on
+    // - What OS you are building for
+    // - An anonymized MAC address and assembly name ID to aggregate the other information on.
     internal class Analytics
     {
         private const string MixPanelToken = "ce0fac19508f6c8f20066d345d360fd0";
