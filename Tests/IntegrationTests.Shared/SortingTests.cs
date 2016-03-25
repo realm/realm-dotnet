295f7e9f7a2f75412724920e171f4dd8161031d0
Andy Dent <dent@oofile.com.au>
2016-03-25 19:58:12 +0800

    #353 Specifying Sort - isolated string sorting

--- Tests/IntegrationTests.Shared/SortingTests.cs
+++ Tests/IntegrationTests.Shared/SortingTests.cs
@@ -60,10 +60,18 @@ namespace IntegrationTests
 
 
         [Test]
-        public void QuerySortOneLevel()
+        public void QuerySortOneLevelNumbers()
         {
             var s0 = _realm.All<Person>().Where(p => p.IsInteresting).OrderBy(p => p.Score).ToList().Select(p => p.Score);
             Assert.That(s0, Is.EqualTo( new float[] {-0.9907f, 42.42f, 100}) );
+        }
+
+
+        [Test]
+        public void QuerySortOneLevelStrings()
+        {
+            var s0 = _realm.All<Person>().Where(p => p.IsInteresting).OrderBy(p => p.LastName).ToList().Select(p => p.LastName);
+            Assert.That(s0, Is.EqualTo( new [] {"Doesmore", "Jameson", "Smith"}) );
         }
 
 
@@ -72,8 +80,11 @@ namespace IntegrationTests
         {
             var sortAA = _realm.All<Person>().Where(p => p.IsInteresting).
                 OrderBy(p => p.FirstName).
-                ThenBy(p => p.Latitude).ToList().Select(p => p.Latitude);
-            Assert.That(sortAA, Is.EqualTo( new double[] {40.7637286, 51.508530, 37.7798657}) );
+                ThenBy(p => p.Latitude).ToList();
+            var sortAAname = sortAA.Select(p => p.FirstName);
+            Assert.That(sortAAname, Is.EqualTo( new [] {"John", "John", "Peter"}) );
+            var sortAAlat = sortAA.Select(p => p.Latitude);
+            Assert.That(sortAAlat, Is.EqualTo( new double[] {40.7637286, 51.508530, 37.7798657}) );
         }
 
 
@@ -82,7 +93,10 @@ namespace IntegrationTests
         {
             var sortDA = _realm.All<Person>().Where(p => p.IsInteresting).
                 OrderByDescending(p => p.FirstName).
-                ThenBy(p => p.Latitude).ToList().Select(p => p.Latitude);
+                ThenBy(p => p.Latitude).ToList();
+            var sortDAname = sortDA.Select(p => p.FirstName);
+            Assert.That(sortDAname, Is.EqualTo( new [] {"Peter", "John", "John"}) );
+            var sortDAlat = sortDA.Select(p => p.Latitude);
             Assert.That(sortDA, Is.EqualTo( new double[] {37.7798657, 40.7637286, 51.508530}) );
         }
 
@@ -92,8 +106,11 @@ namespace IntegrationTests
         {
             var sortAD = _realm.All<Person>().Where(p => p.IsInteresting).
                 OrderBy(p => p.FirstName).
-                ThenByDescending(p => p.Latitude).ToList().Select(p => p.Latitude);
-            Assert.That(sortAD, Is.EqualTo( new double[] {51.508530, 40.7637286, 37.7798657}) );
+                ThenByDescending(p => p.Latitude).ToList();
+            var sortADname = sortAD.Select(p => p.FirstName);
+            Assert.That(sortADname, Is.EqualTo( new [] {"John", "John", "Peter"}) );
+            var sortADlat = sortAD.Select(p => p.Latitude);
+            Assert.That(sortADlat, Is.EqualTo( new double[] {51.508530, 40.7637286, 37.7798657}) );
         }
 
 
@@ -102,8 +119,11 @@ namespace IntegrationTests
         {
             var sortDD = _realm.All<Person>().Where(p => p.IsInteresting).
                 OrderByDescending(p => p.FirstName).
-                ThenByDescending(p => p.Latitude).ToList().Select(p => p.Latitude);
-            Assert.That(sortDD, Is.EqualTo( new double[] {37.7798657, 51.508530, 40.7637286}) );
+                ThenByDescending(p => p.Latitude).ToList();
+            var sortDDname = sortDD.Select(p => p.FirstName);
+            Assert.That(sortDDname, Is.EqualTo( new [] {"Peter", "John", "John"}) );
+            var sortDDlat = sortDD.Select(p => p.Latitude);
+            Assert.That(sortDDlat, Is.EqualTo( new double[] {37.7798657, 51.508530, 40.7637286}) );
         }
 
         //TODO some exception tests for misuse of clauses
