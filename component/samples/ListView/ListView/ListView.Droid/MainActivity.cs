4834cd47ff8677a9f9fd60fce108cc7ce1fa0798
Yavor Georgiev <fealebenpae@gmail.com>
2016-04-20 15:59:10 +0200

    Add Xamarin.Forms ListView example for component

---
+++ component/samples/ListView/ListView/ListView.Droid/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Xamarin.Forms.Platform.Android;
+
+namespace Realms.XamarinForms.Examples.ListView.Droid
+{
+    [Activity(MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            FormsAppCompatActivity.TabLayoutResource = Resource.Layout.tabs;
+            FormsAppCompatActivity.ToolbarResource = Resource.Layout.toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ component/samples/ListView/ListView/ListView/App.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+
+namespace Realms.XamarinForms.Examples.ListView
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            // delete any stale data
+            Realm.DeleteRealm(RealmConfiguration.DefaultConfiguration);
+        }
+    }
+}
