bb943b49da20638cb39284b2286b6c5887c2f306
Kristian Dupont <kristian@kristiandupont.com>
2016-04-26 10:42:37 +0200

    Add IRealmObjectHelpers to FakeForWeaverTests

---
+++ Tests/WeaverTests/Realm.FakeForWeaverTests/IRealmObjectHelper.cs
@@ -0,0 +1,14 @@
+﻿/* Copyright 2016 Realm Inc - All Rights Reserved
+ * Proprietary and Confidential
+ */
+
+using System;
+
+namespace Realms.Weaving
+{
+    public interface IRealmObjectHelper
+    {
+        RealmObject CreateInstance();
+    }
+}
+
