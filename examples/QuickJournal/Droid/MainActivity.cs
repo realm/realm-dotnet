d820d3732a294970cd9e5a0938a5a3c63807f12e
Yavor Georgiev <fealebenpae@gmail.com>
2016-05-19 15:20:43 +0200

    Remove icon on Android

--- examples/QuickJournal/Droid/MainActivity.cs
+++ examples/QuickJournal/Droid/MainActivity.cs
@@ -10,15 +10,16 @@ using Android.OS;
 
 namespace QuickJournal.Droid
 {
-    [Activity(Label = "QuickJournal.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "QuickJournal.Droid", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+            ActionBar.SetIcon(null);
 
+            global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
     }
