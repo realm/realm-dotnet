6ba0324f876cc68cfb113ffcd92afebfb36ed666
Kristian Dupont <kristian@kristiandupont.com>
2016-05-19 11:34:00 +0200

    Add QuickJournal solution

---
+++ examples/QuickJournal/QuickJournal/JournalEntriesPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace QuickJournal
+{
+    public partial class JournalEntriesPage : ContentPage
+    {
+        public JournalEntriesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
