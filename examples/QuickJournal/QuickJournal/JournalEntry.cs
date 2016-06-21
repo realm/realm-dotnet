7b0dc4d854b6eab2e054012fa525658ab5aa62d1
Yavor Georgiev <fealebenpae@gmail.com>
2016-06-21 13:37:52 +0300

    Add Title property to QuickJournal example (#648)
    This demonstrates the new support for `INotifyPropertyChanged` in the weaver.

--- examples/QuickJournal/QuickJournal/JournalEntry.cs
+++ examples/QuickJournal/QuickJournal/JournalEntry.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.ComponentModel;
 using Realms;
 
 namespace QuickJournal
 {
-    public class JournalEntry : RealmObject
+    public class JournalEntry : RealmObject, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Title { get; set; }
         public DateTimeOffset Date { get; set; }
         public string BodyText { get; set; }
     }
