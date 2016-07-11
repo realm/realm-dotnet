92e13d506161a841dec78ca8aa33b9e9aba18a8b
Andy Dent <dent@oofile.com.au>
2016-07-11 22:52:33 +0800

    #287 IList Declarations - reverse check for non-Realm classes in IList mistakenly removed

--- RealmWeaver/ModuleWeaver.cs
+++ RealmWeaver/ModuleWeaver.cs
@@ -307,6 +307,13 @@ public class ModuleWeaver
         // IList or RealmList allows people to declare lists only of _realmObject due to the class definition
         else if (prop.PropertyType.Name == "IList`1" && prop.PropertyType.Namespace == "System.Collections.Generic") {
             var elementType = ((GenericInstanceType)prop.PropertyType).GenericArguments.Single();
+            if (!elementType.Resolve().BaseType.IsSameAs(_realmObject)) {
+                LogWarningPoint(
+  $"SKIPPING {type.Name}.{columnName} because it is an IList but its generic type is not a RealmObject subclass, so will not persist",
+  sequencePoint);
+                return;
+            }
+
             if (prop.SetMethod != null) {
                 LogErrorPoint(
                     $"{type.Name}.{columnName} has a setter but its type is a IList which only supports getters",
