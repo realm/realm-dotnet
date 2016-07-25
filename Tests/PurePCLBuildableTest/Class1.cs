66a0d8b25fb8a2e97047f3619ed5057ec7cdbdaf
Andy Dent <Andy Dent>
2016-07-25 17:04:58 +0800

    #715 Fix Weaving error
    PurePCLBuildableTest.csproj
    - Class1.cs filled in RealmObject subclass using IList
    
    ModuleWeaver.csproj
    - Execute - import System_IList directly - fix to use List<> type

--- Tests/PurePCLBuildableTest/Class1.cs
+++ Tests/PurePCLBuildableTest/Class1.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Realms;
 
 namespace PurePCLBuildableTest
 {
-    public class Class1
+    public class Class1 : RealmObject
     {
+        public string Name { get; set; }
+        public IList<Class1> Siblings { get; }
     }
 }
