2baf0135aa7c9cd2eb363f5cfdfd1d96456ee4ad
Kristian Dupont <kristian@kristiandupont.com>
2016-08-09 13:09:15 +0200

    Extract nano seconds per tick into a constant

--- wrappers/src/timestamp_helpers.hpp
+++ wrappers/src/timestamp_helpers.hpp
@@ -9,12 +9,13 @@
 
 const int64_t unix_epoch_ticks = 621355968000000000;
 const int64_t ticks_per_second = 10000000;
+const int64_t nanoseconds_per_tick = 100;
 
 inline int64_t to_ticks(const realm::Timestamp& ts)
 {
     const int64_t seconds = ts.get_seconds();
     const int32_t nanoseconds = ts.get_nanoseconds();
-    const int64_t ticks = seconds * ticks_per_second + nanoseconds / 100 + unix_epoch_ticks;
+    const int64_t ticks = seconds * ticks_per_second + nanoseconds / nanoseconds_per_tick + unix_epoch_ticks;
     return ticks;
 }
 
@@ -22,7 +23,7 @@ inline realm::Timestamp from_ticks(int64_t ticks)
 {
     const int64_t unix_ticks = ticks - unix_epoch_ticks;
     const int64_t seconds = unix_ticks / ticks_per_second;
-    const int32_t nanoseconds = unix_ticks % ticks_per_second * 100;
+    const int32_t nanoseconds = unix_ticks % ticks_per_second * nanoseconds_per_tick;
     return realm::Timestamp(seconds, nanoseconds);
 }
 
