e6492db3ec06e5eb4d78baf3a9e71dcf71faf296
Kristian Dupont <kristian@kristiandupont.com>
2016-10-06 16:35:57 -0700

    Fix indentation in entire TestRunner file

--- Tests/IntegrationTests.Shared/TestRunner.cs
+++ Tests/IntegrationTests.Shared/TestRunner.cs
@@ -23,26 +23,26 @@ using NUnit.Framework.Internal;
 
 namespace IntegrationTests.Shared
 {
-	internal class TestRunner
-	{
-		internal static void Run(string title, Stream outputStream)
-		{
-			var suite = new TestSuite (title);
-			var builder = new NUnitLiteTestAssemblyBuilder ();
-			suite.Add (builder.Build(System.Reflection.Assembly.GetExecutingAssembly(), new Dictionary<string, object>()));
+    internal class TestRunner
+    {
+        internal static void Run(string title, Stream outputStream)
+        {
+            var suite = new TestSuite(title);
+            var builder = new NUnitLiteTestAssemblyBuilder();
+            suite.Add(builder.Build(System.Reflection.Assembly.GetExecutingAssembly(), new Dictionary<string, object>()));
 
-			var testExecutionContext = TestExecutionContext.CurrentContext;
-			testExecutionContext.WorkDirectory = Environment.CurrentDirectory;
+            var testExecutionContext = TestExecutionContext.CurrentContext;
+            testExecutionContext.WorkDirectory = Environment.CurrentDirectory;
 
-            var workItem = suite.CreateWorkItem (TestFilter.Empty, null);
-            workItem.Execute (testExecutionContext);
+            var workItem = suite.CreateWorkItem(TestFilter.Empty, null);
+            workItem.Execute(testExecutionContext);
 
-			var testWriter = new NUnitLite.Runner.NUnit2XmlOutputWriter (DateTime.Now);
-			using (var writer = new StreamWriter (outputStream)) 
-			{
-				testWriter.WriteResultFile (workItem.Result, writer);
-			}
-		}
-	}
+            var testWriter = new NUnitLite.Runner.NUnit2XmlOutputWriter(DateTime.Now);
+            using (var writer = new StreamWriter(outputStream))
+            {
+                testWriter.WriteResultFile(workItem.Result, writer);
+            }
+        }
+    }
 }
 
