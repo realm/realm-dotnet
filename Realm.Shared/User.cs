7c400a26acf7d7b764c36f9954091013908efe02
Kristian Dupont <kristian@kristiandupont.com>
2016-10-07 16:04:51 -0700

    Add unimplemented classes

---
+++ Realm.Shared/User.cs
@@ -0,0 +1,62 @@
+﻿////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2016 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Threading.Tasks;
+
+namespace Realms
+{
+    public class User
+    {
+        public static User GetCurrentUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static User FromJson()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static User Authenticate(Credentials credentials, string serverUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static async Task<User> AuthenticateAsync(Credentials credentials, string serverUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AccessToken { get; private set; }
+
+        public string Identity { get; private set; }
+
+        public bool IsValid { get; private set; }
+
+        public void LogOut()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ToJson()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
