bab2cabf79c7105ac795983ff9c175f580100afa
Yavor Georgiev <yavor.georgiev@outlook.com>
2016-11-10 12:31:59 +0100

    user login and refresh token

--- Shared/Realm.Sync.Shared/Handles/SharedRealmHandleExtensions.cs
+++ Shared/Realm.Sync.Shared/Handles/SharedRealmHandleExtensions.cs
@@ -33,23 +33,51 @@ namespace Realms.Sync
                 byte[] encryptionKey,
                 out NativeException ex);
 
+            public unsafe delegate void RefreshAccessTokenCallbackDelegate(IntPtr user_handle_ptr, IntPtr session_handle_ptr, sbyte* url_buf, IntPtr url_len);
+
             [DllImport(InteropConfig.DLL_NAME, EntryPoint = "realm_initialize_sync", CallingConvention = CallingConvention.Cdecl)]
-            public static extern void initialize_sync([MarshalAs(UnmanagedType.LPWStr)] string base_path, IntPtr base_path_leth);
+            public static extern void initialize_sync([MarshalAs(UnmanagedType.LPWStr)] string base_path, IntPtr base_path_leth, RefreshAccessTokenCallbackDelegate refresh_callback);
         }
 
-        public static IntPtr OpenWithSync(this SharedRealmHandle sharedRealmHandle, Realms.Native.Configuration configuration, Native.SyncConfiguration syncConfiguration, RealmSchema schema, byte[] encryptionKey)
+        static unsafe SharedRealmHandleExtensions()
+        {
+            InitializeSync(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), RefreshAccessTokenCallback);
+        }
+
+        public static SharedRealmHandle OpenWithSync(Realms.Native.Configuration configuration, Native.SyncConfiguration syncConfiguration, RealmSchema schema, byte[] encryptionKey)
         {
             var marshaledSchema = new SharedRealmHandle.SchemaMarshaler(schema);
 
             NativeException nativeException;
             var result = NativeMethods.open_with_sync(configuration, syncConfiguration, marshaledSchema.Objects, marshaledSchema.Objects.Length, marshaledSchema.Properties, encryptionKey, out nativeException);
             nativeException.ThrowIfNecessary();
-            return result;
+
+            var handle = new SharedRealmHandle();
+            handle.SetHandle(result);
+            return handle;
         }
 
-        public static void InitializeSync(string basePath)
+        private static void InitializeSync(string basePath, NativeMethods.RefreshAccessTokenCallbackDelegate refreshCallback)
         {
-            NativeMethods.initialize_sync(basePath, (IntPtr)basePath.Length);
+            NativeMethods.initialize_sync(basePath, (IntPtr)basePath.Length, refreshCallback);
+        }
+
+        #if __IOS__
+        [ObjCRuntime.MonoPInvokeCallback(typeof(NativeMethods.RefreshAccessTokenCallbackDelegate))]
+        #endif
+        private static unsafe void RefreshAccessTokenCallback(IntPtr userHandlePtr, IntPtr sessionHandlePtr, sbyte* urlBuffer, IntPtr urlLength)
+        {
+            var userHandle = new SyncUserHandle();
+            userHandle.SetHandle(userHandlePtr);
+            var user = new User(userHandle);
+
+            var realmUri = new Uri(new string(urlBuffer, 0, (int)urlLength, System.Text.Encoding.UTF8));
+
+            user.RefreshAccessToken(realmUri.AbsolutePath)
+                .ContinueWith(t =>
+            {
+                var accessToken = t.Result;
+            });
         }
     }
 }
