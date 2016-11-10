e59061882739e850b27efd7a512dd11b353fefc3
Andy Dent <dent@oofile.com.au>
2016-11-10 16:43:21 +0800

    DrawXNative iOS sample drawing paths

---
+++ examples/DrawXNative/DrawXShared/RealmDrawerMedia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SkiaSharp;
+namespace DrawXShared
+{
+    public static class RealmDrawerMedia
+    {
+        // copied from Draw sample SwatchColor.m
+        public static class Colors
+        {
+            public static readonly SKColor Charcoal = new SKColor(28, 35, 63); 
+            public static readonly SKColor Elephant = new SKColor(154, 155, 165); 
+            public static readonly SKColor Dove = new SKColor(235, 235, 242); 
+            public static readonly SKColor Ultramarine = new SKColor(57, 71, 127); 
+            public static readonly SKColor Indigo = new SKColor(89, 86, 158); 
+            public static readonly SKColor GrapeJelly = new SKColor(154, 80, 165); 
+            public static readonly SKColor Mulberry = new SKColor(211, 76, 163); 
+            public static readonly SKColor Flamingo = new SKColor(242, 81, 146); 
+            public static readonly SKColor SexySalmon = new SKColor(247, 124, 136); 
+            public static readonly SKColor Peach = new SKColor(252, 159, 149); 
+            public static readonly SKColor Melon = new SKColor(252, 195, 151); 
+        }
+
+    }
+}
