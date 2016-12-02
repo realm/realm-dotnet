1f87aae1c06c41bcc9849fa8f003230112c184a4
Nikola Irinchev <irinchev@me.com>
2016-12-02 16:27:36 +0100

    Fix IL generation for Realm.Add

--- Weaver/RealmWeaver.Fody/ModuleWeaver.cs
+++ Weaver/RealmWeaver.Fody/ModuleWeaver.cs
@@ -753,6 +753,7 @@ public class ModuleWeaver
                         il.Append(il.Create(OpCodes.Ldfld, field));
                         il.Append(il.Create(OpCodes.Ldarg_2));
                         il.Append(il.Create(OpCodes.Call, new GenericInstanceMethod(_realmAddGenericReference) { GenericArguments = { field.FieldType } }));
+                        il.Append(il.Create(OpCodes.Pop));
                     }
                     else if (!property.IsNullable())
                     {
