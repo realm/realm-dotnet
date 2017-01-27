4195cd1434add5149d6e448c6fae7fedf803277d
Nikola Irinchev <irinchev@me.com>
2017-01-27 11:43:51 +0100

    Release 0.82.1 (#1188)
    * Revert sync/win32 bits
    
    * Add changelog entry and update OS to tg/cache-schema-check
    
    * Bump assembly version
    
    * Update OS reference to point to master
    
    * Update test apps to 0.82.1
    
    * Fix quickjournal crash
    
    * Revert "Revert sync/win32 bits"
    
    This reverts commit 75eafe0c06f3c5e0b71cc0f9b7ba0975bc17e4b9.

--- RealmAssemblyInfo.cs
+++ RealmAssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Reflection;
 [assembly: AssemblyCompany("Realm Inc.")]
 [assembly: AssemblyProduct("Realm C#")]
 
-[assembly: AssemblyVersion("0.83.0.0")]
-[assembly: AssemblyFileVersion("0.83.0.0")]
+[assembly: AssemblyVersion("0.82.1.0")]
+[assembly: AssemblyFileVersion("0.82.1.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
