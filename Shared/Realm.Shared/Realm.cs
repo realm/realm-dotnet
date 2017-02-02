9627274b48d2efadeb74d35969c61989d1ee0d85
Nikola Irinchev <irinchev@me.com>
2017-02-02 02:41:44 +0100

    Refresh the realm after WriteAsync

--- Shared/Realm.Shared/Realm.cs
+++ Shared/Realm.Shared/Realm.cs
@@ -25,6 +25,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.ConstrainedExecution;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 #if __IOS__
 using ObjCRuntime;
@@ -308,7 +309,7 @@ namespace Realms
         {
             if (Error == null)
             {
-                Console.Error.WriteLine("A realm-level exception has occurred. To handle and react to those, subscribe to the Realm.Error event.");
+                ErrorMessages.OutputError(ErrorMessages.RealmNotifyErrorNoSubscribers);
             }
 
             Error?.Invoke(this, new ErrorEventArgs(ex));
@@ -718,7 +719,7 @@ namespace Realms
         /// Action to perform inside a <see cref="Transaction"/>, creating, updating, or removing objects.
         /// </param>
         /// <returns>An awaitable <see cref="Task"/>.</returns>
-        public Task WriteAsync(Action<Realm> action)
+        public async Task WriteAsync(Action<Realm> action)
         {
             ThrowIfDisposed();
 
@@ -729,7 +730,8 @@ namespace Realms
 
             // avoid capturing `this` in the lambda
             var configuration = Config;
-            return Task.Run(() =>
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            await Task.Run(() =>
             {
                 using (var realm = GetInstance(configuration))
                 using (var transaction = realm.BeginWrite())
@@ -738,6 +740,15 @@ namespace Realms
                     transaction.Commit();
                 }
             });
+
+            if (Thread.CurrentThread.ManagedThreadId == threadId)
+            {
+                Refresh();
+            }
+            else
+            {
+                ErrorMessages.OutputError(ErrorMessages.RealmWriteAsyncThreadChange);
+            }
         }
 
         /// <summary>
