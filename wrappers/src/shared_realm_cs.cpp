d103b6d5a5a1d2515876b9f00e840f41ac9cc49e
Nikola Irinchev <irinchev@me.com>
2017-03-09 14:53:12 +0100

    remove win from nuget

--- wrappers/src/shared_realm_cs.cpp
+++ wrappers/src/shared_realm_cs.cpp
@@ -65,7 +65,8 @@ namespace binding {
                 if (change.kind == CSharpBindingContext::ColumnInfo::Kind::Set) {
                     auto const& observed_object_details = static_cast<ObservedObjectDetails*>(o.info);
                     
-                    if (toRemove.find(observed_object_details->managed_object_handle) == toRemove.end() &&
+                    if (!observed_object_details->schema.name.empty() &&
+                        toRemove.find(observed_object_details->managed_object_handle) == toRemove.end() &&
                         !notify_realm_object_changed(observed_object_details->managed_object_handle, get_property_index(observed_object_details->schema, change.initial_column_index))) {
                         toRemove.insert(observed_object_details->managed_object_handle);
                     }
