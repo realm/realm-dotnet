d006c2425bba3a60240453fa6dee92fe5744b502
Andy Dent <dentaroo@gmail.com>
2017-03-29 19:24:17 +0800

    UWP Runner copying realm dll but not finding due nested reference

--- Tests/Tests.XamarinUWP/TestLocal.cs
+++ Tests/Tests.XamarinUWP/TestLocal.cs
@@ -4,15 +4,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Realms;
 
 namespace NUnit.Tests.Simple {
+
+    class People : RealmObject
+    {
+        public string Name { get; set; }
+    }
+
+
     [TestFixture]
     public class TestLocal {
         [Test]
         public void TestMethod()
         {
             // TODO: Add your test code here
-            Assert.Pass("Your first passing test");
+            using (var realm = Realm.GetInstance())
+            {
+                var initialCount = realm.All<People>().Count();
+
+                realm.Write(() =>
+                {
+                    realm.Add(new People() {Name = "Andy"});
+                });
+                Assert.Equals(initialCount + 1, realm.All<People>().Count());
+            }
         }
     }
 }
