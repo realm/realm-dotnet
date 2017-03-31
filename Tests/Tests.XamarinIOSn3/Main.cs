3489fc4c49aad171107d7fdb6fb8fc2bebfb1dad
Andy Dent <dent@oofile.com.au>
2017-03-31 12:07:27 +0800

    Renamed new IOS tests so can have both projects side by side

---
+++ Tests/Tests.XamarinIOSn3/Main.cs
@@ -0,0 +1,48 @@
+﻿////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2016 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.IO;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace IntegrationTests.XamarinIOSn3
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        private static void Main(string[] args)
+        {
+            if (NSProcessInfo.ProcessInfo.Arguments.Any("--headless".Equals))
+            {
+                using (var output = File.OpenWrite(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "TestResults.iOS.xml")))
+                {
+                   ////ASD TODO IntegrationTests.TestRunner.Run("iOS", output);
+                }
+
+                return;
+            }
+
+            // if you want to use a different Application Delegate class from "UnitTestAppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "UnitTestAppDelegate");
+        }
+    }
+}
