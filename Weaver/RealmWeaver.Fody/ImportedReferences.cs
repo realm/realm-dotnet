706e2094ec54b927ffce9115c028221a559e8c97
Nikola Irinchev <irinchev@me.com>
2017-04-28 01:40:03 +0300

    Convert databinding project to netstandard

--- Weaver/RealmWeaver.Fody/ImportedReferences.cs
+++ Weaver/RealmWeaver.Fody/ImportedReferences.cs
@@ -422,7 +422,7 @@ namespace RealmWeaver
                         var dataBindingAssembly = module.AssemblyReferences.SingleOrDefault(r => r.Name == "Realm.DataBinding");
                         if (dataBindingAssembly == null)
                         {
-                            dataBindingAssembly = new AssemblyNameReference("Realm.DataBinding", new Version(1, 0, 0, 0));
+                            dataBindingAssembly = new AssemblyNameReference("Realm.DataBinding", new Version(1, 1, 0, 0));
                             module.AssemblyReferences.Add(dataBindingAssembly);
                         }
                         references.InitializeDataBinding(dataBindingAssembly);
