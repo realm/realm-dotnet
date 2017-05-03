59d6a7b1f58705da69bcd726f2de376260dc9b8e
Nikola Irinchev <irinchev@me.com>
2017-05-04 02:09:06 +0300

    Add Manual Nuget runners (#1370)
    * Add iOS and Android nuget runners
    
    * Add windows and UWP manual tests

--- Manual Tests/NuGetReleaseTests/Tests.Android/Constants.cs
+++ Manual Tests/NuGetReleaseTests/Tests.Android/Constants.cs
@@ -16,23 +16,11 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
-using System;
-using System.Linq;
-using System.Collections.Generic;
-
-using Foundation;
-using UIKit;
-
-namespace NuGetReleaseTests
+namespace Tests
 {
-    public class Application
+    public static class Constants
     {
-        // This is the main entry point of the application.
-        static void Main (string [] args)
-        {
-            // if you want to use a different Application Delegate class from "UnitTestAppDelegate"
-            // you can specify it here.
-            UIApplication.Main (args, null, "UnitTestAppDelegate");
-        }
+        public const string ActivityLabel = "NuGet Tests";
+        public const string PackageName = "io.realm.nugettests";
     }
 }
