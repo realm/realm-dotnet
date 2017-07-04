8863216fd992ee33f6f8a3b404c819fad87dda36
Nikola Irinchev <irinchev@me.com>
2017-07-04 13:25:11 +0200

    wip

--- Tests/Tests.NetCore/Program.cs
+++ Tests/Tests.NetCore/Program.cs
@@ -33,31 +33,31 @@ namespace Tests
     {
         static void Main(string[] args)
         {
-			var cmd = new CommandLineApplication(throwOnUnexpectedArg: false);
-			var regex = cmd.Option("--where", "Specify a RegEx to filter test cases", CommandOptionType.SingleValue);
+            var cmd = new CommandLineApplication(throwOnUnexpectedArg: false);
+            var regex = cmd.Option("--where", "Specify a RegEx to filter test cases", CommandOptionType.SingleValue);
             var force = cmd.Option("-f | --force", "Specify whether the test should be run even if it's marked [Explicit]", CommandOptionType.NoValue);
-			cmd.HelpOption("-? | -h | --help");
+            cmd.HelpOption("-? | -h | --help");
 
             cmd.OnExecute(() =>
             {
-				var runner = new NUnitTestAssemblyRunner(new DefaultTestAssemblyBuilder());
-				runner.Load(typeof(Program).GetTypeInfo().Assembly, new Dictionary<string, object>());
+                var runner = new NUnitTestAssemblyRunner(new DefaultTestAssemblyBuilder());
+                runner.Load(typeof(Program).GetTypeInfo().Assembly, new Dictionary<string, object>());
 
                 var result = runner.Run(new CustomTestListener(cmd), new CustomTestFilter(regex.Value(), force.HasValue()));
 
-				var total = result.FailCount + result.PassCount + result.InconclusiveCount;
-				var message = $"Tests run: {total} Passed: {result.PassCount} Failed: {result.FailCount} Inconclusive: {result.InconclusiveCount}";
+                var total = result.FailCount + result.PassCount + result.InconclusiveCount;
+                var message = $"Tests run: {total} Passed: {result.PassCount} Failed: {result.FailCount} Inconclusive: {result.InconclusiveCount}";
                 cmd.Out.WriteLine(message);
 
                 return 0;
-			});
+            });
 
             cmd.Execute(args);
         }
     }
 
     internal class CustomTestListener : ITestListener
-	{
+    {
         private readonly CommandLineApplication _cmd;
 
         public CustomTestListener(CommandLineApplication cmd)
@@ -66,68 +66,68 @@ namespace Tests
         }
 
         public void TestFinished(ITestResult result)
-		{
-			if (!result.Test.IsSuite)
-			{
-				var className = result.Test.ClassName?.Split('.').LastOrDefault();
-				var status = result.ResultState.Status.ToString().ToUpper();
-				var message = $"\t[{status}] {className}.{result.Test.Name}";
+        {
+            if (!result.Test.IsSuite)
+            {
+                var className = result.Test.ClassName?.Split('.').LastOrDefault();
+                var status = result.ResultState.Status.ToString().ToUpper();
+                var message = $"\t[{status}] {className}.{result.Test.Name}";
                 _cmd.Out.WriteLine(message);
-			}
-		}
+            }
+        }
 
-		public void TestOutput(TestOutput output)
-		{
-		}
+        public void TestOutput(TestOutput output)
+        {
+        }
 
-		public void TestStarted(ITest test)
-		{
-		}
-	}
+        public void TestStarted(ITest test)
+        {
+        }
+    }
 
     internal class CustomTestFilter : TestFilter
-	{
-		private readonly Regex _testRegex;
-		private readonly bool _force;
+    {
+        private readonly Regex _testRegex;
+        private readonly bool _force;
 
-		public CustomTestFilter(string regex, bool force)
-		{
+        public CustomTestFilter(string regex, bool force)
+        {
             if (!string.IsNullOrEmpty(regex))
             {
                 _testRegex = new Regex(regex);
             }
 
-			_force = force;
-		}
+            _force = force;
+        }
 
-		public override TNode AddToXml(TNode parentNode, bool recursive)
-		{
-			return parentNode.AddElement("filter");
-		}
+        public override TNode AddToXml(TNode parentNode, bool recursive)
+        {
+            return parentNode.AddElement("filter");
+        }
 
-		public override bool Match(ITest test)
-		{
+        public override bool Match(ITest test)
+        {
             if (_testRegex?.IsMatch(test.FullName) != false)
             {
-				// We don't want to run explicit tests
-				if (!_force)
-				{
-					var parent = test;
-					while (parent != null)
-					{
-						if (parent.RunState != RunState.Runnable)
-						{
-							return false;
-						}
+                // We don't want to run explicit tests
+                if (!_force)
+                {
+                    var parent = test;
+                    while (parent != null)
+                    {
+                        if (parent.RunState != RunState.Runnable)
+                        {
+                            return false;
+                        }
 
-						parent = parent.Parent;
-					}
-				}
+                        parent = parent.Parent;
+                    }
+                }
 
-				return true;
-			}
+                return true;
+            }
 
-			return false;
-		}
-	}
+            return false;
+        }
+    }
 }
