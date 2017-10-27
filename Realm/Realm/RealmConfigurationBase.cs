1752f7a0d3421a55b82233b22c437baf149e83ce
Nikola Irinchev <irinchev@me.com>
2017-10-27 15:31:59 +0200

    Expose parameterless RealmConfigurationBase ctor

--- Realm/Realm/RealmConfigurationBase.cs
+++ Realm/Realm/RealmConfigurationBase.cs
@@ -118,12 +118,13 @@ namespace Realms
             }
         }
 
-        internal RealmConfigurationBase(string optionalPath = null, bool calculatePath = true)
+        internal RealmConfigurationBase()
         {
-            if (calculatePath)
-            {
-                DatabasePath = GetPathToRealm(optionalPath);
-            }
+        }
+
+        internal RealmConfigurationBase(string optionalPath)
+        {
+            DatabasePath = GetPathToRealm(optionalPath);
         }
 
         internal abstract Realm CreateRealm(RealmSchema schema);
