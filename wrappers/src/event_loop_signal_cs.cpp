aa0dd6b08b2fb7e7cd42bae4d350b39f6a8699d0
Yavor Georgiev <fealebenpae@users.noreply.github.com>
2017-11-07 23:12:13 +0200

    Update our GenericEventLoop implementation (#1627)

--- wrappers/src/event_loop_signal_cs.cpp
+++ wrappers/src/event_loop_signal_cs.cpp
@@ -16,19 +16,55 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
-#include <utility>
+#include <functional>
 #include "util/generic/event_loop_signal.hpp"
 #include "realm_export_decls.hpp"
 
 using namespace realm::util;
 
+using GetContextT = void*();
+using PostOnContextT = void(void* context, void(*handler)(void* user_data), void* user_data);
+using ReleaseContextT = void(void* context);
+
+struct SynchronizationContextEventLoop : public GenericEventLoop {
+public:
+    SynchronizationContextEventLoop(void* context, PostOnContextT* post, ReleaseContextT* release)
+    : m_context(context)
+    , m_post(post)
+    , m_release(release)
+    { }
+
+    void post(std::function<void()> action) override
+    {
+        m_post(m_context, &handle, new std::function<void()>(std::move(action)));
+    }
+
+    ~SynchronizationContextEventLoop()
+    {
+        m_release(m_context);
+    }
+private:
+    static void handle(void* user_data)
+    {
+        auto& func = *reinterpret_cast<std::function<void()>*>(user_data);
+        func();
+        delete &func;
+    }
+
+    void* m_context;
+
+    PostOnContextT* m_post;
+    ReleaseContextT* m_release;
+};
+
 extern "C" {
 
-REALM_EXPORT void realm_install_eventloop_callbacks(decltype(s_get_eventloop) get, decltype(s_post_on_eventloop) post, decltype(s_release_eventloop) release)
+REALM_EXPORT void realm_install_eventloop_callbacks(GetContextT* get, PostOnContextT* post, ReleaseContextT* release)
 {
-    s_get_eventloop = get;
-    s_post_on_eventloop = post;
-    s_release_eventloop = release;
+    GenericEventLoop::set_event_loop_factory([=] {
+        auto* eventloop = new SynchronizationContextEventLoop(get(), post, release);
+        return std::unique_ptr<GenericEventLoop>(eventloop);
+    });
 }
 
 }
