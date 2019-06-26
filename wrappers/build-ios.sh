936a3f5c6bdafb993c0e022e4eb15929426565a6
Nikola Irinchev <irinchev@me.com>
2019-06-26 21:53:10 +0200

    Fix @rpath/ magic

--- wrappers/build-ios.sh
+++ wrappers/build-ios.sh
@@ -27,6 +27,13 @@ function build() {
     -DCMAKE_XCODE_ATTRIBUTE_DYLIB_INSTALL_NAME_BASE="@rpath" \
     -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY \
     -DCMAKE_IOS_INSTALL_COMBINED=YES
+
+  # This is a workaround for CMAKE_IOS_INSTALL_COMBINED removing @rpath from LC_DYLIB_ID.
+  # Reported here: https://cmake.org/pipermail/cmake/2018-October/068316.html
+  rm build/iOS/$REALM_CMAKE_CONFIGURATION/realm-wrappers.framework/realm-wrappers
+  xcrun lipo -create cmake/iOS/src/$REALM_CMAKE_CONFIGURATION-iphoneos/realm-wrappers.framework/realm-wrappers \
+                     cmake/iOS/src/$REALM_CMAKE_CONFIGURATION-iphonesimulator/realm-wrappers.framework/realm-wrappers \
+             -output build/iOS/$REALM_CMAKE_CONFIGURATION/realm-wrappers.framework/realm-wrappers
 }
 
 build
