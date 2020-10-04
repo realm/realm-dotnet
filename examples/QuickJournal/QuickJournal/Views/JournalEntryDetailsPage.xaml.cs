a610150ea6c23998bf44fbc28e2c3c14a1fdc59d
Nikola Irinchev <irinchev@me.com>
2020-10-04 14:01:52 -0700

    Update quickjournal (#2057)
    * Remove quickjournal
    
    * Add new quickjournal
    
    * Update changelog

--- examples/QuickJournal/QuickJournal/Views/JournalEntryDetailsPage.xaml.cs
+++ examples/QuickJournal/QuickJournal/Views/JournalEntryDetailsPage.xaml.cs
@@ -3,7 +3,6 @@ using Xamarin.Forms.Xaml;
 
 namespace QuickJournal
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class JournalEntryDetailsPage : ContentPage
     {
         public JournalEntryDetailsPage(JournalEntryDetailsViewModel viewModel)
