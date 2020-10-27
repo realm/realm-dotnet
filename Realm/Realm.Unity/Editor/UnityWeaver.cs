ee89ed7e633590663a1f19db06ab9d8bd0e15787
Nikola Irinchev <irinchev@me.com>
2020-10-27 17:47:46 +0100

    Assembly cleanup

--- Realm/Realm.Unity/Editor/UnityWeaver.cs
+++ Realm/Realm.Unity/Editor/UnityWeaver.cs
@@ -1,9 +1,25 @@
-﻿using System.Collections.Generic;
+﻿////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2020 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+using System;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Runtime.Versioning;
-using System.Text;
 using JetBrains.Annotations;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
@@ -35,16 +51,11 @@ namespace RealmWeaver
         [InitializeOnLoadMethod]
         private static void Initialize()
         {
-            CompilationPipeline.assemblyCompilationFinished -= ComplicationComplete;
-            CompilationPipeline.assemblyCompilationFinished += ComplicationComplete;
+            CompilationPipeline.assemblyCompilationFinished -= CompilationComplete;
+            CompilationPipeline.assemblyCompilationFinished += CompilationComplete;
         }
 
-        private static void ComplicationComplete(string assemblyPath, CompilerMessage[] compilerMessages)
-        {
-            WeaveAssembly(assemblyPath);
-        }
-
-        private static void WeaveAssembly(string assemblyPath)
+        private static void CompilationComplete(string assemblyPath, CompilerMessage[] compilerMessages)
         {
             if (string.IsNullOrEmpty(assemblyPath))
             {
@@ -75,10 +86,7 @@ namespace RealmWeaver
 
                 moduleDefinition.Write(_writerParameters);
 
-                var report = new StringBuilder();
-                report.AppendLine($"[{name}] Weaving completed in {timer.ElapsedMilliseconds} ms.");
-                report.AppendLine(results.ToString());
-                logger.Info(report.ToString());
+                logger.Info($"[{name}] Weaving completed in {timer.ElapsedMilliseconds} ms.{Environment.NewLine}{results}");
             }
 
             // save any changes to our weavedAssembly objects
