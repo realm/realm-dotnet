e8184b0d1e91ce8dbc55ac7fcc141e5ba7879562
Nikola Irinchev <irinchev@me.com>
2020-11-16 00:53:46 +0100

    Final polish - Use nightly build for Benchmark.NET (to prepare for Xamarin support) - Add a few write tests - Add readme

--- Tests/PerformanceTests/QueryTests.cs
+++ Tests/PerformanceTests/QueryTests.cs
@@ -19,30 +19,26 @@
 using System;
 using System.Linq;
 using BenchmarkDotNet.Attributes;
-using Bogus;
 using Realms;
 
 namespace PerformanceTests
 {
     public class QueryTests : BenchmarkBase
     {
-        private int _executionCounter;
-
         [Params(10, 100, 1000)]
         public int ObjectCount { get; set; }
 
         protected override void SeedData()
         {
-            var dataset = new DataSet();
             _realm.Write(() =>
             {
                 for (var i = 0; i < ObjectCount; i++)
                 {
                     _realm.Add(new QueryClass
                     {
-                        StringValue = dataset.Random.Utf16String(40, 40),
+                        StringValue = _faker.Random.Utf16String(40, 40),
                         BoolValue = i % 2 == 0,
-                        IntValue = dataset.Random.Int()
+                        IntValue = _faker.Random.Int()
                     });
                 }
             });
@@ -51,14 +47,14 @@ namespace PerformanceTests
         [Benchmark]
         public int Count()
         {
-            var expectedBool = _executionCounter++ % 2 == 0;
+            var expectedBool = _faker.Random.Bool();
             return _realm.All<QueryClass>().Where(c => c.BoolValue == expectedBool).Count();
         }
 
         [Benchmark]
         public object Enumerate()
         {
-            var expectedBool = _executionCounter++ % 2 == 0;
+            var expectedBool = _faker.Random.Bool();
             return _realm.All<QueryClass>().Where(c => c.BoolValue == expectedBool).ToArray();
         }
 
