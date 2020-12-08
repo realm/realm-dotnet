94ffbe29839f6853674f4a800159c1914b9af1ff
Andrea Catalini <andrea.catalini89@gmail.com>
2020-12-08 17:01:01 +0100

    Fixed issue with AssemblyInfo.cs

--- Tools/SetupUnityPackage/UnityUtils/Properties/AssemblyInfo.cs
+++ Tools/SetupUnityPackage/UnityUtils/Properties/AssemblyInfo.cs
@@ -1,32 +1,25 @@
-﻿using System.Reflection;
+﻿////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2020 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+
 using UnityEngine.Scripting;
-using System.Runtime.CompilerServices;
-
-// Information about this assembly is defined by the following attributes. 
-// Change them to the values specific to your project.
-
-[assembly: AssemblyTitle("UnityUtils")]
-[assembly: AssemblyDescription("Utils to support the Realm SDK in Unity projects")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Realm")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Force UnityLinker to process this assembly regardless of whether or not
-// it is referenced by other assemblies
-// more info at https://docs.unity3d.com/ScriptReference/Scripting.AlwaysLinkAssemblyAttribute.html
+// it is referenced by other assemblies.
+// More info at https://docs.unity3d.com/ScriptReference/Scripting.AlwaysLinkAssemblyAttribute.html
 [assembly: AlwaysLinkAssembly]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
