fea840956d3cba80020a3168a6cfe3ae9383fa43
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2021-01-15 16:50:04 +0100

    First step in argument removal

--- Tests/Benchmarks/PerformanceTests/WriteTests.cs
+++ Tests/Benchmarks/PerformanceTests/WriteTests.cs
@@ -24,69 +24,14 @@ using Realms;
 
 namespace PerformanceTests
 {
-    public class WriteTests : BenchmarkBase
+    public abstract class WriteTests : BenchmarkBase
     {
-        private const int MaxObjectCount = 100;
-        private const int Bytes_128B = 128;
-        private const int Bytes_1KB = 1024;
-        private const int Bytes_1MB = 1024 * 1024;
+        [Params(10, 100)]
+        public int ObjectCount { get; set; }
 
-        private IDictionary<int, InsertClass[]> _stringObjects;
-        private IDictionary<int, InsertClass[]> _binaryObjects;
+        protected const int MaxObjectCount = 100;
 
-        protected override void SeedData()
-        {
-            _stringObjects = new[] { 20, 200 }.ToDictionary(o => o, GenerateStringObjects);
-            _binaryObjects = new[] { Bytes_128B, Bytes_1KB, Bytes_1MB }.ToDictionary(o => o, GenerateBinaryObjects);
-        }
-
-        [Benchmark]
-        public void EmptyWrite()
-        {
-            using var transaction = _realm.BeginWrite();
-            transaction.Commit();
-        }
-
-        [Benchmark]
-        [Arguments(10, 20)]
-        [Arguments(100, 20)]
-        [Arguments(10, 200)]
-        [Arguments(100, 200)]
-        public void Strings(int objectCount, int stringLength)
-        {
-            var objects = _stringObjects[stringLength];
-            _realm.Write(() =>
-            {
-                _realm.Add(_stringObjects[stringLength].Take(objectCount));
-            });
-        }
-
-        [Benchmark]
-        [Arguments(10, Bytes_128B)]
-        [Arguments(100, Bytes_128B)]
-        [Arguments(10, Bytes_1KB)]
-        [Arguments(100, Bytes_1KB)]
-        [Arguments(10, Bytes_1MB)]
-        [Arguments(100, Bytes_1MB)]
-        public void Binary(int objectCount, int binarySize)
-        {
-            _realm.Write(() =>
-            {
-                _realm.Add(_binaryObjects[binarySize].Take(objectCount));
-            });
-        }
-
-        private InsertClass[] GenerateStringObjects(int stringSize)
-            => Enumerable.Range(0, MaxObjectCount)
-            .Select(_ => new InsertClass { StringValue = _faker.Random.Utf16String(stringSize, stringSize) })
-            .ToArray();
-
-        private InsertClass[] GenerateBinaryObjects(int binarySize)
-            => Enumerable.Range(0, MaxObjectCount)
-            .Select(_ => new InsertClass { Bytes = _faker.Random.Bytes(binarySize) })
-            .ToArray();
-
-        private class InsertClass : RealmObject
+        protected class InsertClass : RealmObject
         {
             public string StringValue { get; set; }
 
@@ -97,6 +42,75 @@ namespace PerformanceTests
             public DateTimeOffset DateValue { get; set; }
 
             public byte[] Bytes { get; set; }
+        }
+    }
+
+    public class EmptyWriteTests : WriteTests
+    {
+        [Benchmark]
+        public void EmptyWrite()
+        {
+            using var transaction = _realm.BeginWrite();
+            transaction.Commit();
+        }
+    }
+
+    public class BinaryWriteTests : WriteTests
+    {
+        private const int Bytes_128B = 128;
+        private const int Bytes_1KB = 1024;
+        private const int Bytes_1MB = 1024 * 1024;
+
+        private IDictionary<int, InsertClass[]> _binaryObjects;
+
+        [Params(Bytes_128B, Bytes_1KB, Bytes_1MB)]
+        public int BinarySize { get; set; }
+
+        protected override void SeedData()
+        {
+            _binaryObjects = new[] { Bytes_128B, Bytes_1KB, Bytes_1MB }.ToDictionary(o => o, GenerateBinaryObjects);
+        }
+
+        [Benchmark]
+        public void Binary()
+        {
+            _realm.Write(() =>
+            {
+                _realm.Add(_binaryObjects[BinarySize].Take(ObjectCount));
+            });
+        }
+
+        private InsertClass[] GenerateBinaryObjects(int binarySize)
+            => Enumerable.Range(0, MaxObjectCount)
+            .Select(_ => new InsertClass { Bytes = _faker.Random.Bytes(binarySize) })
+            .ToArray();
+    }
+
+    public class StringWriteTests : WriteTests
+    {
+        private IDictionary<int, InsertClass[]> _stringObjects;
+
+        [Params(20, 200)]
+        public int StringLength { get; set; }
+
+        protected override void SeedData()
+        {
+            _stringObjects = new[] { 20, 200 }.ToDictionary(o => o, GenerateStringObjects);
+        }
+
+        private InsertClass[] GenerateStringObjects(int stringSize)
+            => Enumerable.Range(0, MaxObjectCount)
+            .Select(_ => new InsertClass { StringValue = _faker.Random.Utf16String(stringSize, stringSize) })
+            .ToArray();
+
+        [Benchmark]
+        public void Strings()
+        {
+            var objects = _stringObjects[StringLength];
+            _realm.Write(() =>
+            {
+                _realm.Add(_stringObjects[StringLength].Take(ObjectCount));
+            });
         }
     }
 }
