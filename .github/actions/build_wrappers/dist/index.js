d06e75d900132c9ad4e6dfe1ae848758aa3764c1
Andrea Catalini <andrea.catalini89@gmail.com>
2021-03-03 12:08:47 +0100

    Reverted to cp.exec

--- .github/actions/build_wrappers/dist/index.js
+++ .github/actions/build_wrappers/dist/index.js
@@ -137,16 +137,19 @@ exports.execShellCommand = void 0;
 const cp = __importStar(__nccwpck_require__(3129));
 function execShellCommand(cmd, outputStream) {
     return new Promise((resolve, reject) => {
-        let buildCmd = cp.spawn(cmd);
-        buildCmd.stdout.on("data", (data) => {
+        var _a, _b;
+        let buildCmd = cp.exec(cmd);
+        outputStream.startGroup(`Child process output`);
+        (_a = buildCmd.stdout) === null || _a === void 0 ? void 0 : _a.on("data", (data) => {
             outputStream.info(`stdout: ${data.toString()}`);
         });
-        buildCmd.stderr.on("data", (data) => {
+        (_b = buildCmd.stderr) === null || _b === void 0 ? void 0 : _b.on("data", (data) => {
             outputStream.error(`stderr: ${data.toString()}`);
         });
         buildCmd.on("exit", (code) => {
             outputStream.info(`Child process exited with code ${code === null || code === void 0 ? void 0 : code.toString()}`);
         });
+        outputStream.endGroup();
     });
 }
 exports.execShellCommand = execShellCommand;
--- .github/actions/build_wrappers/src/utils/common.ts
+++ .github/actions/build_wrappers/src/utils/common.ts
@@ -5,20 +5,27 @@ export interface output {
     info(message: string): void;
     warning(message: string): void;
     error(message: string): void;
+    startGroup(name: string): void;
+    endGroup(): void;
 }
 
 export function execShellCommand(cmd: string, outputStream: output): Promise<void>
 {
     return new Promise((resolve, reject) => {
-        let buildCmd = cp.spawn(cmd);
-        buildCmd.stdout.on("data", (data) => {
+        let buildCmd = cp.exec(cmd);
+
+        outputStream.startGroup(`Child process output`);
+
+        buildCmd.stdout?.on("data", (data) => {
             outputStream.info(`stdout: ${data.toString()}`);
         });
-        buildCmd.stderr.on("data", (data) => {
+        buildCmd.stderr?.on("data", (data) => {
             outputStream.error(`stderr: ${data.toString()}`);
         });
         buildCmd.on("exit", (code) =>{
             outputStream.info(`Child process exited with code ${code?.toString()}`);
         });
+
+        outputStream.endGroup();
     });
 }
