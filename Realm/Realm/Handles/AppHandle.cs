702cd252028124b352be8244730bee3d1b6f4d18
Nikola Irinchev <irinchev@me.com>
2021-03-31 11:23:11 +0200

    Disable warnings and update the SDK version (#2315)
    * Disable warnings and update the SDK version
    
    * Remove expected warnings
    
    * Use the correct hardcoded version

--- Realm/Realm/Handles/AppHandle.cs
+++ Realm/Realm/Handles/AppHandle.cs
@@ -175,7 +175,9 @@ namespace Realms.Sync
 
             var platform = InteropConfig.Platform;
             var platformVersion = RuntimeInformation.OSDescription;
-            var sdkVersion = InteropConfig.SDKVersion.ToString(3);
+
+            // TODO: temp - remove that once we're out of beta
+            var sdkVersion = "10.2.0-beta.1"; // InteropConfig.SDKVersion.ToString(3);
 
             NativeMethods.initialize(
                 platform, platform.IntPtrLength(),
