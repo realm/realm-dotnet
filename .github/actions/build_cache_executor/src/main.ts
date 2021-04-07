b8e7bd724f24b4836553264da55dc1cbafa7baa9
Nikola Irinchev <irinchev@me.com>
2021-04-07 23:18:54 +0200

    Revert files that are unrelated to the workflow change

--- .github/actions/build_cache_executor/src/main.ts
+++ .github/actions/build_cache_executor/src/main.ts
@@ -2,18 +2,22 @@ import * as core from "@actions/core";
 import * as actionCore from "../../../ga_packages/build_cache_executor/dist/build_cache_executor";
 
 async function run(): Promise<void> {
-    try {
-        const inputPath = core.getInput("inputPath", { required: true });
-        const outputPath = core.getInput("outputPath", { required: true });
-        const cmd = core.getInput("cmd", { required: true });
-        const outputVar = core.getInput("outputVar");
+  try {
+    const path = core.getInput("cachePath", { required: true });
+    const cmd = core.getInput("cmd", { required: true });
 
-        const cacheKey = await actionCore.actionCore(inputPath, outputPath, cmd, core);
+    const cacheKey = await actionCore.actionCore(path, cmd, core);
 
-        core.setOutput(outputVar, cacheKey);
-    } catch (error) {
-        core.setFailed(error.message);
+    if (cacheKey === undefined) {
+      core.setFailed(
+        `Action aborted because either artifacts could not be built or they could not be cached`
+      );
+      return;
     }
+    core.setOutput("hashKey", cacheKey);
+  } catch (error) {
+    core.setFailed(`Hard failure: ${error.message}`);
+  }
 }
 
 run();
--- .github/ga_packages/build_cache_executor/src/build_cache_executor.ts
+++ .github/ga_packages/build_cache_executor/src/build_cache_executor.ts
@@ -6,25 +6,64 @@ import * as fs from "fs-extra";
 /**
  * Builds and caches the resulting artifacts. In order to store the artifacts in a cache, a hash (cacheKey) is calculated over paths and the result is used as key in the cache dictionary.
  * The function can throw exceptions.
- * @param inputPath Path that needs to be cached after the build (same paths used to create a hash)
+ * @param path Path that needs to be cached after the build (same paths used to create a hash)
  * @param cmd Cmd to execute to obtain a build
  * @param logger Output stream where to print the messages
  * @returns CacheKey necessary to recover the cached build later on. Undefined is returned if something went wrong.
  */
-export async function actionCore(inputPath: string, outputPath: string, cmd: string, logger: Logger): Promise<string | undefined> {
-    await validateInput(inputPath, outputPath, cmd);
+export async function actionCore(path: string, cmd: string, logger: iLogger): Promise<string | undefined> {
+    if (cmd.length === 0) {
+        throw new Error(`No command was supplied, nothing to do.`);
+    }
+    if (path.length === 0) {
+        throw new Error(`No path was supplied, nothing to cache.`);
+    }
 
-    const pathHash = (await folderHash.hashElement(inputPath)).hash;
-    const hashKey = `${cmd}${pathHash}`;
+    let hashKey: string | undefined;
+    try {
+        hashKey = cmd.concat(await getHash(path));
+    } catch (err) {
+        throw new Error(`While calculating the hash something went terribly wrong: ${err.message}`);
+    }
 
-    logger.info(`Hash key for ${inputPath} is: ${hashKey}`);
+    let cacheHit: string | undefined = undefined;
+    if (hashKey !== undefined) {
+        logger.info(`Hash key for ${path} is: ${hashKey}`);
+        try {
+            cacheHit = await cache.restoreCache([path], hashKey);
+        } catch (err) {
+            logger.error(`Impossible to retrieve cache: ${err}\n The build will start momentarily...`);
+        }
+    } else {
+        throw new Error(
+            `No hash could be calculated, so nothing to search in cache. Since what's going to be built now can't be cached, abort!`
+        );
+    }
 
-    if (!await existsInCache(outputPath, hashKey)) {
-        logger.info("No cache was found, so the command will be executed...");
+    if (cacheHit === undefined) {
+        logger.info(`No cache was found, so the command will be executed...`);
 
-        await executeCommand(cmd);
+        try {
+            const returnCode = await exec.exec(cmd);
+            if (returnCode !== 0) {
+                throw Error(`Executing a command ${cmd} failed with code ${returnCode}. Stopping execution!`);
+            }
+        } catch (err) {
+            throw new Error(`Something went terribly wrong while executing a shell command: ${err.message}`);
+        }
 
-        await cache.saveCache([outputPath], hashKey);
+        if (hashKey !== undefined) {
+            try {
+                const cacheId = await cache.saveCache([path], hashKey);
+                logger.info(`Cache properly created with id ${cacheId}`);
+            } catch (error) {
+                throw new Error(`The cache could not be saved: ${error.message}`);
+            }
+        } else {
+            throw new Error(
+                `HashKey was undefined, so the current build can't be save. This should have never happened!`
+            );
+        }
     } else {
         logger.info(`A build was found in cache for hashKey ${hashKey}\nskipping building...`);
     }
@@ -32,51 +71,24 @@ export async function actionCore(inputPath: string, outputPath: string, cmd: str
     return hashKey;
 }
 
-async function validateInput(inputPath: string, outputPath: string, cmd: string): Promise<void> {
-    if (cmd.length === 0) {
-        throw new Error(`No command was supplied, nothing to do.`);
-    }
-
-    if (inputPath.length === 0) {
-        throw new Error("No inputPath was supplied");
-    }
-
-    if (!(await fs.pathExists(inputPath))) {
-        throw new Error(`inputPath '${inputPath}' doesn't exist`);
-    }
-
-    if (outputPath.length === 0) {
-        throw new Error("No outputPath was supplied");
-    }
-}
-
-async function existsInCache(outputPath: string, key: string): Promise<boolean> {
-    try {
-        const cacheHit = await cache.restoreCache([outputPath], key);
-        return cacheHit === key;
-    } catch { }
-
-    return false;
-}
-
-async function executeCommand(cmd: string): Promise<void> {
-    let returnCode = 0;
-    try {
-        returnCode = await exec.exec(cmd);
-    } catch (err) {
-        throw new Error(`Something went wrong while executing the command '${cmd}': ${err.message}`);
-    }
-
-    if (returnCode !== 0) {
-        throw Error(`Executing a command '${cmd}' failed with non-zero code: ${returnCode}`);
-    }
-}
-
-export interface Logger {
+export interface iLogger {
     debug(message: string): void;
     info(message: string): void;
     warning(message: string): void;
     error(message: string): void;
+}
+
+/** @internal */
+// Given a path, it calculates a hash resulting from the joined hashes of all subfolders and subfiles.
+// Can throw exceptions.
+export async function getHash(path: string): Promise<string> {
+    if (path.length === 0) {
+        throw new Error("There is no path supplied");
+    }
+    if (!(await fs.pathExists(path))) {
+        throw new Error(`${path} path doesn't exist`);
+    }
+    return (await folderHash.hashElement(path)).hash;
 }
 
 //// DEBUG!: uncomment to debug
