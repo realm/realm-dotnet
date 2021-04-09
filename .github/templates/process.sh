4e87b316f5fa7499a40afe5465036bade3933f8b
Nikola Irinchev <irinchev@me.com>
2021-04-09 17:09:43 +0200

    Remove process.sh

--- .github/templates/process.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-ytt -f main.yml --output-files "${SCRIPT_DIRECTORY}/../workflows"
-
-sed -i '' 's/true:/on:/' "${SCRIPT_DIRECTORY}/../workflows/main.yml"
