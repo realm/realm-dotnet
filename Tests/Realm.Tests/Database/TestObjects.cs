5d35c9da50f8e975c8e25deec0ade7e292f26d11
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2021-05-12 07:53:26 +0200

    Added tests for embedded objects in lists and dictionaries (not supported for sets)

--- Tests/Realm.Tests/Database/TestObjects.cs
+++ Tests/Realm.Tests/Database/TestObjects.cs
@@ -458,6 +458,8 @@ namespace Realms.Tests
 
         public IList<IntPropertyObject> ObjectList { get; }
 
+        public IList<EmbeddedIntPropertyObject> EmbeddedObjectList { get; }
+
         public IList<RealmValue> RealmValueList { get; }
 
         public ISet<char> CharSet { get; }
@@ -526,7 +528,7 @@ namespace Realms.Tests
 
         public IDictionary<string, IntPropertyObject> ObjectDict { get; }
 
-        public IDictionary<string, EmbeddedIntPropertyObject> EmbeddedObjectDictionary { get; }
+        public IDictionary<string, EmbeddedIntPropertyObject> EmbeddedObjectDict { get; }
 
         public IDictionary<string, RealmValue> RealmValueDict { get; }
     }
