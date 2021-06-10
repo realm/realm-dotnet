30eac594042bba55e3ac57ec890312bba0b7f8d6
Andrea Catalini <ac_workrelated@tutanota.com>
2021-06-10 18:27:11 +0200

    Check iphoneToSimulate value - 0

--- .github/actions/run-ios-simulator/src/main.ts
+++ .github/actions/run-ios-simulator/src/main.ts
@@ -19,7 +19,8 @@ async function run(): Promise<void> {
             runtimeId = exec.exec("xcrun simctl list runtimes | awk \'/com.apple.CoreSimulator.SimRuntime.iOS/ { match($0, /\\([0-9.]+ - [a-zA-Z0-9]+\\)/); print substr($0, RSTART + 1, RLENGTH - 2); exit }\'");
         }
 
-        exec.exec("xcrun", ["simctl", "create", id, "com.apple.CoreSimulator.SimDeviceType." + iphoneToSimulate, runtimeId.toString()]);
+        // exec.exec("xcrun", ["simctl", "create", id, "com.apple.CoreSimulator.SimDeviceType." + iphoneToSimulate, runtimeId.toString()]);
+        exec.exec("xcrun", ["simctl", "create", id, "com.apple.CoreSimulator.SimDeviceType.iPhone-8", runtimeId.toString()]);
         exec.exec("xcrun", ["simctl", "boot", id]);
         exec.exec("xcrun", ["simctl", "install", id, appPath]);
         exec.exec("xcrun", ["simctl", "launch", "--console-pty", id, bundleId, args]);
