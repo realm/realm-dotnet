0a33c64e34d6cf064eb014751920a7406e3747f5
Nikola Irinchev <irinchev@me.com>
2021-06-14 23:51:45 +0200

    Combine the changelog and remove beta versions from analytics and apphandle

--- Realm/Realm.Weaver/Analytics.cs
+++ Realm/Realm.Weaver/Analytics.cs
@@ -115,8 +115,7 @@ namespace RealmWeaver
                     .Replace("%SYNC_ENABLED%", _config.IsUsingSync.ToString())
 
                     // Version of weaver is expected to match that of the library.
-                    // TODO: temp - remove that when we're out of beta
-                    .Replace("%REALM_VERSION%", "10.2.0-beta.3") // Assembly.GetExecutingAssembly().GetName().Version.ToString())
+                    .Replace("%REALM_VERSION%", Assembly.GetExecutingAssembly().GetName().Version.ToString())
 
                     .Replace("%OS_TYPE%", osName)
                     .Replace("%OS_VERSION%", osVersion)
