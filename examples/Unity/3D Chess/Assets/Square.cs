f5509d4106419c441cfa9f7628fdfa26eeee3562
Dominic Frei <business.dominic.frei@gmail.com>
2021-06-28 16:26:45 +0100

    Add colliders for the squares.

---
+++ examples/Unity/3D Chess/Assets/Square.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log("Square clicked.");
+    }
+}
