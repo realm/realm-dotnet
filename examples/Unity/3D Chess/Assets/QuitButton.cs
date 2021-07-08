0909fa31d3535c0e9d2647503463b7ebe41c270e
Dominic Frei <business.dominic.frei@gmail.com>
2021-07-08 16:15:17 +0100

    Update to v10.3.0 and add a quit button.

---
+++ examples/Unity/3D Chess/Assets/QuitButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
