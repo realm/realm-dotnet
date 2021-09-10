6c6d16addc670e27757a2b5f5166f1c1711ed181
Nikola Irinchev <irinchev@me.com>
2021-09-10 22:32:30 +0200

    fix some UWP ps1 errors

--- Tests/Tests.UWP/RunTests.ps1
+++ Tests/Tests.UWP/RunTests.ps1
@@ -1,13 +1,13 @@
 param(
-    [string]$PackageLocation = "$PSScriptRoot/AppPackages/Tests.UWP_1.0.0.0_Test",
+    [string]$PackageLocation = "$PSScriptRoot/AppPackages/Tests.UWP_1.0.0.0_Test"
 )
 
-if (-not Test-Path -Path $PackageLocation) {
+if (-not (Test-Path -Path $PackageLocation)) {
     Write-Output "Tests package not found at $PackageLocation"
     exit 1
 }
 
-if (-not Test-Path -Path "$PackageLocation/Install.ps1") {
+if (-not (Test-Path -Path "$PackageLocation/Install.ps1")) {
     Write-Output "Install.ps1 not found at $PackageLocation/Install.ps1"
     exit 2
 }
@@ -15,8 +15,8 @@ if (-not Test-Path -Path "$PackageLocation/Install.ps1") {
 & $PackageLocation/Install.ps1 -Force
 
 $PackagePath = get-appxpackage -name realm.uwp.tests | select -expandproperty PackageFamilyName
-$ResultsPath = $env:LOCALAPPDATA/Packages/$PackagePath/LocalState/TestResults.UWP.xml
-$RunOutputPath = $env:LOCALAPPDATA/Packages/$PackagePath/LocalState/TestRunOutput.txt
+$ResultsPath = "$env:LOCALAPPDATA/Packages/$PackagePath/LocalState/TestResults.UWP.xml"
+$RunOutputPath = "$env:LOCALAPPDATA/Packages/$PackagePath/LocalState/TestRunOutput.txt"
 
 $TestAppProcess = Start-Process shell:AppsFolder\$PackagePath!App -ArgumentList "--headless --labels=After --result=TestResults.UWP.xml" -PassThru -RedirectStandardOutput pwshout.txt -RedirectStandardError pwsherr.txt
 Write-Output "The test application is launched, this step is monitoring it and it will terminate when the tests are fully run"
