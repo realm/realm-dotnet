0ae397a4426990f638fa1e256a4c15c07d808a21
Nikola Irinchev <irinchev@me.com>
2021-10-07 17:38:32 +0200

    Tweaks to the RemoveApps script

--- Tests/Scripts/RemoveApps.ps1
+++ Tests/Scripts/RemoveApps.ps1
@@ -1,5 +1,13 @@
+param (
+    [Parameter(Mandatory=$true)][string]$ClusterName
+)
+
 $existingApps = (& realm-cli --profile deployment -f json -y apps list --disable-colors) | ConvertFrom-Json
-$existingApps.data | ForEach-Object {
-    $appId = $_.Split(" ")[0]
-    & realm-cli --profile deployment -f json -y --disable-colors apps delete -a $appId
+if ($existingApps.data) {
+    $existingApps.data | ForEach-Object {
+        $appId = $_.Split(" ")[0]
+        if ($appId -Match $ClusterName) {
+            & realm-cli --profile deployment -f json -y --disable-colors apps delete -a $appId
+        }
+    }
 }
