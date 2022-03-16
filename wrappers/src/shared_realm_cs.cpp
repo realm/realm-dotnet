9991e104c8661a350a2847f79b430976a28af761
Yavor Georgiev <fealebenpae@gmail.com>
2022-03-16 15:06:38 +0100

    Update to latest core

--- wrappers/src/shared_realm_cs.cpp
+++ wrappers/src/shared_realm_cs.cpp
@@ -201,7 +201,7 @@ REALM_EXPORT SharedRealm* shared_realm_open(Configuration configuration, SchemaO
         if (configuration.read_only) {
             config.schema_mode = SchemaMode::Immutable;
         } else if (configuration.delete_if_migration_needed) {
-            config.schema_mode = SchemaMode::ResetFile;
+            config.schema_mode = SchemaMode::HardResetFile;
         }
 
         if (objects_length > 0) {
