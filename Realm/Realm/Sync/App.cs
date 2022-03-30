031878a01199aa7b4061c040ed81326dc11376dd
Nikola Irinchev <irinchev@me.com>
2022-03-31 00:49:26 +0200

    commit forgotten file

--- Realm/Realm/Sync/App.cs
+++ Realm/Realm/Sync/App.cs
@@ -153,7 +153,7 @@ namespace Realms.Sync
                 }
             }
 
-            var httpClient = new HttpClient(config.HttpClientHandler);
+            var httpClient = config.HttpClientHandler == null ? new HttpClient() : new HttpClient(config.HttpClientHandler);
             var clientHandle = GCHandle.Alloc(httpClient);
             var nativeConfig = new Native.AppConfiguration
             {
