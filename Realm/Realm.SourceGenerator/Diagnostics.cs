6316656fafc40204ad398ce87088a41583f10786
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2022-06-16 15:01:00 +0200

    Fixes for diagnostic

--- Realm/Realm.SourceGenerator/Diagnostics.cs
+++ Realm/Realm.SourceGenerator/Diagnostics.cs
@@ -23,6 +23,8 @@ namespace Realm.SourceGenerator
     internal static class Diagnostics
     {
 
+        #region Errors
+
         public static Diagnostic MultiplePrimaryKeys(string className, Location location)
         {
             DiagnosticDescriptor descriptor = new
@@ -36,24 +38,12 @@ namespace Realm.SourceGenerator
             return Diagnostic.Create(descriptor, location);
         }
 
-        public static Diagnostic DictionaryWithNonStringKeys(string className, string propertyName, string keyType, string valueType, Location location)
-        {
-            DiagnosticDescriptor descriptor = new
-                ("REALM001",
-                "Dictionaries can only have strings as keys",
-                $"{className}.{propertyName} is a Dictionary <{keyType}, {valueType}> but only string keys are currently supported by Realm.",
-                "RealmClassGeneration",
-                DiagnosticSeverity.Error,
-                true);
-            return Diagnostic.Create(descriptor, location);
-        }
-
         public static Diagnostic IndexedWrongType(string className, string propertyName, string propertyType, Location location)
         {
             DiagnosticDescriptor descriptor = new
                 ("REALM001",
                 "[Indexed] is only allowed on specific types",
-                $"{className}.{propertyName} is marked as [Indexed] which is only allowed on integral types as well as string, bool and DateTimeOffset, not on {2}.",
+                $"{className}.{propertyName} is marked as [Indexed] which is only allowed on integral types as well as string, bool and DateTimeOffset, not on {propertyType}.",
                 "RealmClassGeneration",
                 DiagnosticSeverity.Error,
                 true);
@@ -84,5 +74,72 @@ namespace Realm.SourceGenerator
 
             return Diagnostic.Create(descriptor, location);
         }
+
+        public static Diagnostic CollectionRealmInteger(string className, string propertyName, string collectionType, Location location)
+        {
+            DiagnosticDescriptor descriptor = new
+                ("REALM001",
+                "Collections of RealmInteger are not allowed",
+                $"{className}.{propertyName} is an {collectionType}<RealmInteger> which is not supported.",
+                "RealmClassGeneration",
+                DiagnosticSeverity.Error,
+                true);
+            return Diagnostic.Create(descriptor, location);
+        }
+
+        public static Diagnostic CollectionUnsupportedType(string className, string propertyName, string collectionType, string elementType, Location location)
+        {
+            DiagnosticDescriptor descriptor = new
+                ("REALM001",
+                "Unsupported element type in collection",
+                $"{className}.{propertyName} is an {collectionType} but its generic type is {elementType} which is not supported by Realm.",
+                "RealmClassGeneration",
+                DiagnosticSeverity.Error,
+                true);
+            return Diagnostic.Create(descriptor, location);
+        }
+
+        public static Diagnostic CollectionWithSetter(string className, string propertyName, string collectionType, Location location)
+        {
+            DiagnosticDescriptor descriptor = new
+                ("REALM001",
+                "Collections cannot have setters",
+                $"{className}.{propertyName} has a setter but its type is a {collectionType} which only supports getters.",
+                "RealmClassGeneration",
+                DiagnosticSeverity.Error,
+                true);
+            return Diagnostic.Create(descriptor, location);
+        }
+
+        public static Diagnostic DictionaryWithNonStringKeys(string className, string propertyName, string keyType, string valueType, Location location)
+        {
+            DiagnosticDescriptor descriptor = new
+                ("REALM001",
+                "Dictionaries can only have strings as keys",
+                $"{className}.{propertyName} is a Dictionary <{keyType}, {valueType}> but only string keys are currently supported by Realm.",
+                "RealmClassGeneration",
+                DiagnosticSeverity.Error,
+                true);
+            return Diagnostic.Create(descriptor, location);
+        }
+
+        #endregion
+
+        #region Warnings
+
+        public static Diagnostic RealmObjectWithoutAutomaticProperty(string className, string propertyName, Location location)
+        {
+            DiagnosticDescriptor descriptor = new
+                ("REALM001",
+                "RealmObject/EmbeddedObject properties usually indicate a relationship",
+                $"{className}.{propertyName} is not an automatic property but its type is a RealmObject/EmbeddedObject which normally indicates a relationship.",
+                "RealmClassGeneration",
+                DiagnosticSeverity.Warning,
+                true);
+
+            return Diagnostic.Create(descriptor, location);
+        }
+
+        #endregion
     }
 }
