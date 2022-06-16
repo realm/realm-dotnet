b655529c068c728cc87e8b6b70a37e68b3a7d4ad
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2022-06-16 16:54:11 +0200

    Added all possible diagnostics [skip ci]

--- Realm/Realm.SourceGenerator/Diagnostics.cs
+++ Realm/Realm.SourceGenerator/Diagnostics.cs
@@ -22,15 +22,53 @@ namespace Realm.SourceGenerator
 {
     internal static class Diagnostics
     {
-
         #region Errors
+
+        public static Diagnostic ExceptionError(Location location)
+        {
+            DiagnosticDescriptor descriptor = new
+                ("REALM001",
+                "Unexpcted error during source generation",
+                $"There was an unexpected error during source generation",
+                "RealmClassGeneration",
+                DiagnosticSeverity.Error,
+                true);
+
+            return Diagnostic.Create(descriptor, location);
+        }
+
+        public static Diagnostic ObjectWithNoProperties(string className, Location location)
+        {
+            DiagnosticDescriptor descriptor = new
+                ("REALM001",
+                "Realm objects need to have properties",
+                $"Class {className} is a Realm object but has no persisted properties",
+                "RealmClassGeneration",
+                DiagnosticSeverity.Error,
+                true);
+
+            return Diagnostic.Create(descriptor, location);
+        }
 
         public static Diagnostic MultiplePrimaryKeys(string className, Location location)
         {
             DiagnosticDescriptor descriptor = new
                 ("REALM001",
                 "Realm classes cannot have multiple primary keys",
-                $"Class {className} has multiple primary keys.",
+                $"Class {className} has more than one property marked with [PrimaryKey].",
+                "RealmClassGeneration",
+                DiagnosticSeverity.Error,
+                true);
+
+            return Diagnostic.Create(descriptor, location);
+        }
+
+        public static Diagnostic EmbeddedObjectWithPrimaryKey(string className, string propertyName, Location location)
+        {
+            DiagnosticDescriptor descriptor = new
+                ("REALM001",
+                "Embedded objects cannot have primary keys",
+                $"Class {className} is an EmbeddedObject but has a primary key {propertyName} defined.",
                 "RealmClassGeneration",
                 DiagnosticSeverity.Error,
                 true);
@@ -67,7 +105,33 @@ namespace Realm.SourceGenerator
             DiagnosticDescriptor descriptor = new
                 ("REALM001",
                 "[Backlink] property must be of type IQueryable",
-                $"{className}.{propertyName} has [Backlink] applied, but it's not IQueryable",
+                $"{className}.{propertyName} has [Backlink] applied, but it's not IQueryable.",
+                "RealmClassGeneration",
+                DiagnosticSeverity.Error,
+                true);
+
+            return Diagnostic.Create(descriptor, location);
+        }
+
+        public static Diagnostic BacklinkWithSetter(string className, string propertyName, Location location)
+        {
+            DiagnosticDescriptor descriptor = new
+                ("REALM001",
+                "[Backlink] property cannot have a setter",
+                $"{className}.{propertyName} has a setter but also has [Backlink] applied, which only supports getters.",
+                "RealmClassGeneration",
+                DiagnosticSeverity.Error,
+                true);
+
+            return Diagnostic.Create(descriptor, location);
+        }
+
+        public static Diagnostic BacklinkWrongRelationship(string className, string propertyName, string elementType, string inversePropertyName, Location location)
+        {
+            DiagnosticDescriptor descriptor = new
+                ("REALM001",
+                "Wrong definition of inverse relationship",
+                $"The property '{elementType}.{inversePropertyName}' does not constitute a link to '{className}' as described by '{className}.{propertyName}'.",
                 "RealmClassGeneration",
                 DiagnosticSeverity.Error,
                 true);
@@ -115,13 +179,77 @@ namespace Realm.SourceGenerator
         {
             DiagnosticDescriptor descriptor = new
                 ("REALM001",
-                "Dictionaries can only have strings as keys",
-                $"{className}.{propertyName} is a Dictionary <{keyType}, {valueType}> but only string keys are currently supported by Realm.",
+                "Embedded objects cannot be used in sets",
+                $"{className}.{propertyName} is a Set<IEmbeddedObject> which is not supported. Embedded objects are always unique which is why List<IEmbeddedObject> already has Set semantics.",
                 "RealmClassGeneration",
                 DiagnosticSeverity.Error,
                 true);
             return Diagnostic.Create(descriptor, location);
         }
+
+        public static Diagnostic SetWithEmbedded(string className, string propertyName, string collectionType, Location location)
+        {
+            DiagnosticDescriptor descriptor = new
+                ("REALM001",
+                "Sets cannot have setters",
+                $"{className}.{propertyName} has a setter but its type is a {collectionType} which only supports getters.",
+                "RealmClassGeneration",
+                DiagnosticSeverity.Error,
+                true);
+            return Diagnostic.Create(descriptor, location);
+        }
+
+        public static Diagnostic ListWithoutInterface(string className, string propertyName, string genericType, Location location)
+        {
+            DiagnosticDescriptor descriptor = new
+                ("REALM001",
+                "List properties must be declared as IList",
+                $"{className}.{propertyName} is declared as List<{genericType}> which is not the correct way to declare to-many relationships in Realm. If you want to persist the collection, use the interface IList<{genericType}>, otherwise annotate the property with the [Ignored] attribute.",
+                "RealmClassGeneration",
+                DiagnosticSeverity.Error,
+                true);
+            return Diagnostic.Create(descriptor, location);
+        }
+
+        public static Diagnostic DateTimeNotSupported(string className, string propertyName, Location location)
+        {
+            DiagnosticDescriptor descriptor = new
+                ("REALM001",
+                "DateTime is not supported",
+                $"{className}.{propertyName} is a DateTime which is not supported - use DateTimeOffset instead.",
+                "RealmClassGeneration",
+                DiagnosticSeverity.Error,
+                true);
+
+            return Diagnostic.Create(descriptor, location);
+        }
+
+        public static Diagnostic NullableDateTimeNotSupported(string className, string propertyName, Location location)
+        {
+            DiagnosticDescriptor descriptor = new
+                ("REALM001",
+                "DateTime? is not supported",
+                $"{className}.{propertyName} is a DateTime? which is not supported - use DateTimeOffset? instead.",
+                "RealmClassGeneration",
+                DiagnosticSeverity.Error,
+                true);
+
+            return Diagnostic.Create(descriptor, location);
+        }
+
+        public static Diagnostic TypeNotSupported(string className, string propertyName, string propertyType, Location location)
+        {
+            DiagnosticDescriptor descriptor = new
+                ("REALM001",
+                "Type not supported",
+                $"{className}.{propertyName} is a is a '{propertyType}' which is not yet supported.",
+                "RealmClassGeneration",
+                DiagnosticSeverity.Error,
+                true);
+
+            return Diagnostic.Create(descriptor, location);
+        }
+
 
         #endregion
 
@@ -133,6 +261,19 @@ namespace Realm.SourceGenerator
                 ("REALM001",
                 "RealmObject/EmbeddedObject properties usually indicate a relationship",
                 $"{className}.{propertyName} is not an automatic property but its type is a RealmObject/EmbeddedObject which normally indicates a relationship.",
+                "RealmClassGeneration",
+                DiagnosticSeverity.Warning,
+                true);
+
+            return Diagnostic.Create(descriptor, location);
+        }
+
+        public static Diagnostic NotPersistedPropertyWithRealmAttributes(string className, string propertyName, Location location)
+        {
+            DiagnosticDescriptor descriptor = new
+                ("REALM001",
+                "Not persisted property with Realm attributes",
+                $"{className}.{propertyName} has one or more Realm attributes applied, but it's not persisted, so those attributes will be ignored.",
                 "RealmClassGeneration",
                 DiagnosticSeverity.Warning,
                 true);
