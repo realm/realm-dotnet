670831625906337182cfcef9cb107b6ed5898735
Nikola Irinchev <irinchev@me.com>
2022-06-23 12:05:43 +0200

    Add a parent node for test suites

--- Tests/Tests.Unity/Assets/TestsManager/TestManager.cs
+++ Tests/Tests.Unity/Assets/TestsManager/TestManager.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework.Interfaces;
 using Realms.Tests;
 using System;
+using System.Globalization;
 using System.Linq;
 using System.Xml;
 using UnityEngine;
@@ -70,11 +71,68 @@ public class TestManager : ITestRunCallback
 
         using (var writer = XmlWriter.Create(resultPath))
         {
-            result.ToXml(recursive: true).WriteTo(writer);
+            var resultNode = result.ToXml(recursive: true);
+            StripSystemOut(resultNode);
+
+            var testNode = MakeTestRunElement(result);
+            testNode.ChildNodes.Add(resultNode);
+
+            testNode.WriteTo(writer);
+
             writer.Flush();
         }
 
         HackyLogger.Log($"Results path is {resultPath}, transforming results to junit.");
         TestHelpers.TransformTestResults(resultPath);
+    }
+
+    // This is deliberately identical to NUnitLite to match other test results we generate:
+    // https://github.com/nunit/nunit/blob/34a39b3f7ce1ae49ed7d7bcddaec9573c6364f97/src/NUnitFramework/nunitlite/OutputWriters/NUnit3XmlOutputWriter.cs#L75
+    private static TNode MakeTestRunElement(ITestResult result)
+    {
+        var testRun = new TNode("test-run");
+
+        testRun.AddAttribute("id", "2");
+        testRun.AddAttribute("name", result.Name);
+        testRun.AddAttribute("fullname", result.FullName);
+        testRun.AddAttribute("testcasecount", result.Test.TestCaseCount.ToString());
+
+        testRun.AddAttribute("result", result.ResultState.Status.ToString());
+        if (result.ResultState.Label != string.Empty)
+            testRun.AddAttribute("label", result.ResultState.Label);
+
+        testRun.AddAttribute("start-time", result.StartTime.ToString("o"));
+        testRun.AddAttribute("end-time", result.EndTime.ToString("o"));
+        testRun.AddAttribute("duration", result.Duration.ToString("0.000000", NumberFormatInfo.InvariantInfo));
+
+        var total = result.FailCount + result.PassCount + result.InconclusiveCount;
+        testRun.AddAttribute("total", total.ToString());
+        testRun.AddAttribute("passed", result.PassCount.ToString());
+        testRun.AddAttribute("failed", result.FailCount.ToString());
+        testRun.AddAttribute("inconclusive", result.InconclusiveCount.ToString());
+        testRun.AddAttribute("skipped", result.SkipCount.ToString());
+        testRun.AddAttribute("asserts", result.AssertCount.ToString());
+
+        return testRun;
+    }
+
+    private static void StripSystemOut(TNode node)
+    {
+        var outputNode = node.ChildNodes.FirstOrDefault(n => n.Name == "output");
+        if (outputNode != null)
+        {
+            var didPass = node.Attributes.FirstOrDefault(a => a.Key == "result").Value == "Passed";
+            if (didPass)
+            {
+                node.ChildNodes.Remove(outputNode);
+            }
+        }
+        else
+        {
+            foreach (var child in node.ChildNodes)
+            {
+                StripSystemOut(child);
+            }
+        }
     }
 }
