203452d3d8f7e5b8ca3cf40b5f3f765c888a6dce
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2022-10-04 09:30:25 +0200

    Corrected generated classes in test assembly

--- Tests/SourceGenerators/SourceGeneratorAssemblyToProcess/Generated/Realm.SourceGenerator/Realms.SourceGenerator.RealmGenerator/RealmObjectAndEmbeddedObjectClass.diagnostics.cs
+++ Tests/SourceGenerators/SourceGeneratorAssemblyToProcess/Generated/Realm.SourceGenerator/Realms.SourceGenerator.RealmGenerator/RealmObjectAndEmbeddedObjectClass.diagnostics.cs
@@ -2,7 +2,7 @@
   {
     "Id": "RLM002",
     "Severity": 3,
-    "Message": "Class RealmObjectAndEmbeddedObjectClass is declared as implementing both IRealmObject and IEmbeddedObject",
+    "Message": "Class RealmObjectAndEmbeddedObjectClass is declared as implementing multiple class interfaces.A class can implement only one interface between IRealmObject, IEmbeddedObject, IAsymmetricObject.",
     "Location": {
       "Path": null,
       "StartLine": 23,
