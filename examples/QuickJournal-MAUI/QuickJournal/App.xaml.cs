f96c0371f251218d377ab84902ef101112744f49
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2023-01-04 13:10:27 +0100

    Created stub for MAUI project

---
+++ examples/QuickJournal-MAUI/QuickJournal/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace QuickJournal;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
+
---
+++ examples/QuickJournal-MAUI/QuickJournal/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QuickJournal;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
+
