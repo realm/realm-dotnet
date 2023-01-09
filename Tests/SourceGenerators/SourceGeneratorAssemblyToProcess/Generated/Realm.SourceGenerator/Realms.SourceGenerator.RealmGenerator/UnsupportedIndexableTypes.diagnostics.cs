7ddcd06308ecdc8ea6d476f761a67ac40a19328b
Nikola Irinchev <irinchev@me.com>
2023-01-09 11:58:41 +0100

    Revert some formatting

--- Tests/SourceGenerators/SourceGeneratorAssemblyToProcess/Generated/Realm.SourceGenerator/Realms.SourceGenerator.RealmGenerator/UnsupportedIndexableTypes.diagnostics.cs
+++ Tests/SourceGenerators/SourceGeneratorAssemblyToProcess/Generated/Realm.SourceGenerator/Realms.SourceGenerator.RealmGenerator/UnsupportedIndexableTypes.diagnostics.cs
@@ -16,83 +16,83 @@
     "Severity": 3,
     "Message": "UnsupportedIndexableTypes.ByteArrayProp is marked as [Indexed] which is only allowed on integral types as well as string, bool and DateTimeOffset, not on byte[].",
     "Location": {
-        "Path": null,
+      "Path": null,
       "StartLine": 27,
       "StartColumn": 9,
       "EndLine": 28,
       "EndColumn": 50
     }
-},
+  },
   {
     "Id": "RLM007",
     "Severity": 3,
     "Message": "UnsupportedIndexableTypes.FloatProp is marked as [Indexed] which is only allowed on integral types as well as string, bool and DateTimeOffset, not on float.",
     "Location": {
-        "Path": null,
+      "Path": null,
       "StartLine": 30,
       "StartColumn": 9,
       "EndLine": 31,
       "EndColumn": 45
     }
-},
+  },
   {
     "Id": "RLM007",
     "Severity": 3,
     "Message": "UnsupportedIndexableTypes.DoubleProp is marked as [Indexed] which is only allowed on integral types as well as string, bool and DateTimeOffset, not on double.",
     "Location": {
-        "Path": null,
+      "Path": null,
       "StartLine": 33,
       "StartColumn": 9,
       "EndLine": 34,
       "EndColumn": 47
     }
-},
+  },
   {
     "Id": "RLM007",
     "Severity": 3,
     "Message": "UnsupportedIndexableTypes.ObjectProp is marked as [Indexed] which is only allowed on integral types as well as string, bool and DateTimeOffset, not on RealmObj.",
     "Location": {
-        "Path": null,
+      "Path": null,
       "StartLine": 36,
       "StartColumn": 9,
       "EndLine": 37,
       "EndColumn": 49
     }
-},
+  },
   {
     "Id": "RLM007",
     "Severity": 3,
     "Message": "UnsupportedIndexableTypes.RealmvalueProp is marked as [Indexed] which is only allowed on integral types as well as string, bool and DateTimeOffset, not on RealmValue.",
     "Location": {
-        "Path": null,
+      "Path": null,
       "StartLine": 39,
       "StartColumn": 9,
       "EndLine": 40,
       "EndColumn": 55
     }
-},
+  },
   {
     "Id": "RLM007",
     "Severity": 3,
     "Message": "UnsupportedIndexableTypes.DecimalProp is marked as [Indexed] which is only allowed on integral types as well as string, bool and DateTimeOffset, not on decimal.",
     "Location": {
-        "Path": null,
+      "Path": null,
       "StartLine": 42,
       "StartColumn": 9,
       "EndLine": 43,
       "EndColumn": 49
     }
-},
+  },
   {
     "Id": "RLM024",
     "Severity": 3,
     "Message": "UnsupportedIndexableTypes.UnsupportedProp is of type 'int[]' which is not yet supported. If that is supposed to be a model class, make sure it implements IRealmObject/IEmbeddedObject/IAsymmetricObject.",
     "Location": {
-        "Path": null,
+      "Path": null,
       "StartLine": 45,
       "StartColumn": 9,
       "EndLine": 46,
       "EndColumn": 51
     }
-}
+  }
 ]
