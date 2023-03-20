9447066af6a6ef6d57aefdbd7e2652c9a9201532
Nikola Irinchev <irinchev@me.com>
2023-03-20 22:24:42 +0100

    add dockerimage and build-linux.sh

---
+++ wrappers/build-linux.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+SCRIPT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+REALM_CMAKE_CONFIGURATION=Debug
+EXTRA_CMAKE_ARGS=""
+
+for i in "$@"
+do
+case $i in
+  -a=*|--ARCH=*)
+    ARCH="${i#*=}"
+    shift
+  ;;
+  -c=*|--configuration=*)
+    REALM_CMAKE_CONFIGURATION="${i#*=}"
+    shift
+  ;;
+  *)
+    EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS $i"
+  ;;
+esac
+done
+
+function build() {
+  if [ $1 == "arm" ]; then
+    EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=\"$SCRIPT_DIRECTORY/realm-core/tools/cmake/armhf.toolchain.cmake\" -DCMAKE_IGNORE_PATH=/usr/lib/x86_64-linux-gnu -DZLIB_LIBRARY=/usr/lib/arm-linux-gnueabihf"
+  elif [ $1 == "arm64" ]; then
+    EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=\"$SCRIPT_DIRECTORY/realm-core/tools/cmake/aarch64.toolchain.cmake\" -DCMAKE_IGNORE_PATH=/usr/lib/x86_64-linux-gnu -DZLIB_LIBRARY=/usr/lib/aarch64-linux-gnu"
+  elif [ $1 != "x64" ]; then
+    echo "Unknown architecture $1"
+    exit 1
+  fi
+
+  bash "$SCRIPT_DIRECTORY"/build.sh -c=$REALM_CMAKE_CONFIGURATION $EXTRA_CMAKE_ARGS
+}
+
+export REALM_CMAKE_CONFIGURATION
+
+if [[ "$ARCH" ]]; then
+  build $ARCH
+else
+  for arch in x64 arm arm64
+  do
+    build $arch
+  done
+fi
---
+++ wrappers/debian-multiarch-arm.Dockerfile
@@ -0,0 +1,29 @@
+FROM debian:11
+
+RUN dpkg --add-architecture armhf && apt-get update
+RUN dpkg --add-architecture arm64 && apt-get update
+RUN apt-get install -y \
+        build-essential \
+        curl \
+        crossbuild-essential-armhf \
+        crossbuild-essential-arm64 \
+        libprocps-dev:armhf \
+        libprocps-dev:arm64 \
+        libssl-dev:armhf \
+        libssl-dev:arm64 \
+        libz-dev:armhf \
+        libz-dev:arm64 \
+        libasio-dev \
+        ninja-build \
+        qemu-user
+
+RUN mkdir -p /opt/cmake \
+    && curl https://cmake.org/files/v3.18/cmake-3.18.2-Linux-x86_64.sh -o /cmake.sh \
+    && sh /cmake.sh --prefix=/opt/cmake --skip-license \
+    && rm /cmake.sh
+
+ENV PATH "/opt/cmake/bin:$PATH"
+
+VOLUME /source
+
+ENTRYPOINT ["/bin/bash"]
