99df2106005c258b4f0fcf23b7a8d5d0d19060f4
Nikola Irinchev <irinchev@me.com>
2023-03-24 23:44:34 +0100

    Annotate Tests.Maui

--- Tests/Realm.Tests/Generated/Realm.SourceGenerator/Realms.SourceGenerator.RealmGenerator/ObjectWithPartitionValue_generated.cs
+++ Tests/Realm.Tests/Generated/Realm.SourceGenerator/Realms.SourceGenerator.RealmGenerator/ObjectWithPartitionValue_generated.cs
@@ -44,7 +44,9 @@ namespace Realms.Tests.Sync
             Realms.Schema.Property.Primitive("Guid", Realms.RealmValueType.Guid, isPrimaryKey: false, isIndexed: false, isNullable: false, managedName: "Guid"),
         }.Build();
 
+        #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         private ObjectWithPartitionValue() {}
+        #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
         #region IRealmObject implementation
 
