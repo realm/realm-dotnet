31633eb6b9ed8c41a7c9b486e65cdfca444bcbf9
Yavor Georgiev <fealebenpae@gmail.com>
2023-07-24 14:51:07 +0200

    fix mac wrappers sdkroot

--- wrappers/build-macos.sh
+++ wrappers/build-macos.sh
@@ -7,5 +7,4 @@ bash "$SCRIPT_DIRECTORY"/build.sh -GXcode \
     -DCMAKE_XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH=NO \
     -DCMAKE_SYSTEM_NAME=Darwin \
     -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" \
-    -destination generic/platform=macOS \
     ${@:1}
